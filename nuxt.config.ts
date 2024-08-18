53576600c01cfc7e6b51b8a8928842103fc690d3
kadeem-lewis <email@kadeemlewis.com>
2024-08-18 17:24:49 -0400

    setup nuxt project with packages I need at the moment

---
+++ nuxt.config.ts
@@ -0,0 +1,17 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: ["@nuxt/ui", "@nuxt/image", "@nuxt/eslint","@vueuse/nuxt"],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  experimental: {
+    typedPages: true,
+  },
+  colorMode: {
+    preference: "system",
+  },
+});

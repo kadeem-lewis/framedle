b621499164b9c8f5122465fbb3e25b9e6a1f88d9
kadeem-lewis <email@kadeemlewis.com>
2024-08-18 19:38:34 -0400

    created tailwind config file

---
+++ tailwind.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from "tailwindcss";
+
+export default <Partial<Config>>{
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}",
+    "./app.vue",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {},
+    },
+  },
+  plugins: [],
+};

35b165ec4b7ab01747998a7c66a5cf0e0472bfce
kadeem-lewis <email@kadeemlewis.com>
2024-08-19 00:35:35 -0400

    created components that will hold component that will be available in modals opened from navbar buttons

---
+++ app/components/content/Settings.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <ColorScheme
+      ><USelect
+        v-model="$colorMode.preference"
+        :options="['system', 'light', 'dark']"
+    /></ColorScheme>
+  </div>
+</template>
+
+<script setup lang="ts"></script>

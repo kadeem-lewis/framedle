d6a7e8a81c2643252acec0e6ea8504f9a9d9c5fe
kadeem-lewis <email@kadeemlewis.com>
2024-08-19 00:34:04 -0400

    removed default component from app

--- app/app.vue
+++ app/app.vue
@@ -1,26 +1,14 @@
+<script setup lang="ts"></script>
+
 <template>
-  <UContainer>
-    <UCard class="mt-10">
-      <template #header>
-        <div class="flex justify-between">
-          <h1>Welcome to Nuxt UI Starter</h1>
-          <ColorScheme
-            ><USelect
-              v-model="$colorMode.preference"
-              :options="['system', 'light', 'dark']"
-          /></ColorScheme>
-        </div>
-      </template>
-      <UButton
-        icon="i-heroicons-book-open"
-        to="https://ui.nuxt.com"
-        target="_blank"
-        >Open Nuxt UI Documentation</UButton
-      >
-    </UCard>
-    <main>
+  <UContainer
+    class="flex min-h-screen flex-col items-center justify-between gap-y-2"
+  >
+    <Navbar />
+    <main class="grow">
       <NuxtPage />
     </main>
+    <Footer />
   </UContainer>
   <UNotifications />
 </template>

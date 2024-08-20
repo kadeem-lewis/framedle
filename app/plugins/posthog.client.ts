a9431af276135a55ca791d3ddb3f0a82d983185d
kadeem-lewis <email@kadeemlewis.com>
2024-08-20 04:35:51 -0400

    disabled posthog dev debugging

--- app/plugins/posthog.client.ts
+++ app/plugins/posthog.client.ts
@@ -6,9 +6,9 @@ export default defineNuxtPlugin(() => {
     api_host: runtimeConfig.public.posthogHost,
     person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
     capture_pageview: false, // we add manual pageview capturing below
-    loaded: (posthog) => {
-      if (import.meta.env.MODE === "development") posthog.debug();
-    },
+    // loaded: (posthog) => {
+    //   if (import.meta.env.MODE === "development") posthog.debug();
+    // },
   });
 
   // Make sure that pageviews are captured with each route change

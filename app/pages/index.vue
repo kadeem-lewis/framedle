722e37128104656d4e7fc5d0c53d4be580b74585
kadeem-lewis <email@kadeemlewis.com>
2024-08-21 04:42:12 -0400

    began styling buttons/cards on home page

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,8 +1,32 @@
 <template>
-  <div>
-    <UButton to="/classic" block square>Classic</UButton>
-    <UButton to="/ability" block square>Ability</UButton>
+  <div class="space-y-4">
+    <p>Daily Guess a Warframe</p>
+    <div class="flex flex-col gap-4">
+      <UCard
+        v-for="tab of tabs"
+        :key="tab.label"
+        :ui="{
+          rounded: false,
+        }"
+        class="uppercase hover:scale-105"
+      >
+        <NuxtLink :to="tab.route">
+          {{ tab.label }}
+        </NuxtLink>
+      </UCard>
+    </div>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const tabs = [
+  {
+    label: "Classic",
+    route: "/classic",
+  },
+  {
+    label: "Ability",
+    route: "/ability",
+  },
+];
+</script>

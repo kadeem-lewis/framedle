4c5e93642fb5dda3975183b70aa41868235997cd
kadeem-lewis <email@kadeemlewis.com>
2024-08-21 04:56:49 -0400

    created a route that will hold past games

---
+++ app/pages/archive.vue
@@ -0,0 +1,9 @@
+<template>
+  <div></div>
+</template>
+
+<script setup lang="ts">
+// Not sure if this would work right since it game mode would need their own archive page
+
+//I could also just have buttons for the different game modes and just have the default be based on the from route
+</script>

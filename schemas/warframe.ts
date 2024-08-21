d5169c68156da4bcaedb6fb8f88e55946de78356
kadeem-lewis <email@kadeemlewis.com>
2024-08-20 21:42:06 -0400

    created a zod schema to be used to validate warframe type

---
+++ schemas/warframe.ts
@@ -0,0 +1,36 @@
+import { z } from "zod";
+
+export const abilitySchema = z.object({
+  uniqueName: z.string(),
+  name: z.string(),
+  description: z.string(),
+  imageName: z.string(),
+});
+
+const progenitorElements = [
+  "Impact",
+  "Toxin",
+  "Electricity",
+  "Radiation",
+  "Magnetic",
+  "Heat",
+  "Cold",
+] as const;
+
+export type Ability = z.infer<typeof abilitySchema>;
+
+export const warframeSchema = z.object({
+  name: z.string(),
+  category: z.literal("Warframes"),
+  type: z.literal("Warframe"),
+  health: z.number(),
+  shield: z.number(),
+  armor: z.number(),
+  releaseDate: z.string(),
+  imageName: z.string(),
+  abilities: z.array(abilitySchema),
+  sex: z.enum(["Male", "Female", "Non-binary (Pluriform)"]),
+  progenitor: z.enum(progenitorElements),
+});
+
+export type Warframe = z.infer<typeof warframeSchema>;

6d34cc26a8d540ef0c8ae446b04329f0c21fde83
kadeem-lewis <email@kadeemlewis.com>
2024-08-20 21:44:02 -0400

    updated api call to perform validation using zod and filter out bad results

--- server/api/warframes.ts
+++ server/api/warframes.ts
@@ -1,15 +1,24 @@
 import Items from "warframe-items";
 import { progenitors } from "../constants/progenitors";
-import type { Warframe } from "~~/types/warframe";
+import { warframeSchema } from "~~/schemas/warframe";
+import type { Warframe } from "~~/schemas/warframe";
 
 export default defineEventHandler(() => {
-  const warframes = new Items({ category: ["Warframes"] })
-    .filter((item) => item.category === "Warframes")
-    .map((item) => ({
-      ...item,
-      progenitor: progenitors[item.name!],
-    })) as Warframe[];
+  const warframes: (Warframe | null)[] = [];
+  const data = new Items({ category: ["Warframes"] });
+  data.forEach((item) => {
+    Object.assign(item, { progenitor: progenitors[item.name!] });
+    const result = warframeSchema.safeParse(item);
+    if (result.success) {
+      warframes.push(result.data);
+    } else {
+      warframes.push(null);
+    }
+  });
+  const filteredWarframes = warframes.filter((warframe) => warframe !== null);
+  console.log(filteredWarframes);
+
   return {
-    warframes,
+    warframes: filteredWarframes,
   };
 });

b8f6bac1ac2269fd4bfc57bf818f45c65e08b790
kadeem-lewis <email@kadeemlewis.com>
2024-08-21 04:41:39 -0400

    created different layouts for different parts of app

---
+++ app/layouts/default.vue
@@ -0,0 +1,30 @@
+<template>
+  <UContainer
+    class="flex min-h-screen flex-col items-center justify-between gap-y-2"
+  >
+    <header class="flex w-full items-center justify-between">
+      <div>
+        <h1 class="text-2xl font-semibold">
+          <NuxtLink to="/">Framedle</NuxtLink>
+        </h1>
+      </div>
+      <div class="flex gap-1">
+        <UButton
+          icon="i-heroicons-cog-6-tooth-solid"
+          variant="ghost"
+          @click="isOpen = true"
+        />
+      </div>
+      <UModal v-model="isOpen"><ContentSettings /></UModal>
+    </header>
+    <main class="grow">
+      <NuxtPage />
+    </main>
+    <Footer />
+  </UContainer>
+  <UNotifications />
+</template>
+
+<script setup lang="ts">
+const isOpen = ref(false);
+</script>
---
+++ app/layouts/game.vue
@@ -0,0 +1,18 @@
+<template>
+  <UContainer
+    class="flex min-h-screen flex-col items-center justify-between gap-y-2"
+  >
+    <Navbar />
+    <main class="grow">
+      <NuxtPage />
+    </main>
+    <Footer />
+  </UContainer>
+  <UNotifications />
+</template>
+
+<script setup lang="ts">
+const { fetchWarframes } = useGameStore();
+
+const { error } = await useAsyncData("warframes", () => fetchWarframes());
+</script>

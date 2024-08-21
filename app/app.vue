2a60b14ccf85ae8bf186006a418c09a322243065
kadeem-lewis <email@kadeemlewis.com>
2024-08-21 04:37:13 -0400

    changed app from being a layout and added seo data to it

--- app/app.vue
+++ app/app.vue
@@ -1,18 +1,17 @@
 <script setup lang="ts">
-const { fetchWarframes } = useGameStore();
-
-const { error } = await useAsyncData("warframes", () => fetchWarframes());
+useHead({
+  title: "Framedle",
+  meta: [
+    {
+      name: "description",
+      content: "Daily Guess a Warframe",
+    },
+  ],
+});
 </script>
 
 <template>
-  <UContainer
-    class="flex min-h-screen flex-col items-center justify-between gap-y-2"
-  >
-    <Navbar />
-    <main class="grow">
-      <NuxtPage />
-    </main>
-    <Footer />
-  </UContainer>
-  <UNotifications />
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>

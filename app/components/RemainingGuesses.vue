9f6e9663dc9adc0dcb686d0016a2e03debde98e7
kadeem-lewis <email@kadeemlewis.com>
2024-08-22 04:22:41 -0400

    created component to display current guesses

---
+++ app/components/RemainingGuesses.vue
@@ -0,0 +1,18 @@
+<template>
+  <div v-if="mode">
+    <p>You have {{ attempts[mode] }} revives left.</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+const { attempts, mode } = storeToRefs(useGameStore());
+
+watch(
+  () => attempts.value[mode.value],
+  () => {
+    if (attempts.value[mode.value] === 0) {
+      isGameOver.value[mode.value] = true;
+    }
+  },
+);
+</script>

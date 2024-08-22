4e7b29f873425251a329d443ea68f705f36d6b5e
kadeem-lewis <email@kadeemlewis.com>
2024-08-22 04:25:35 -0400

    updated ability route to begin applying styling to image to guess

--- app/pages/ability.vue
+++ app/pages/ability.vue
@@ -1,37 +1,39 @@
 <template>
-  <div v-if="mode" class="flex flex-col gap-4">
-    <UTabs v-model="selectedTab" :items="tabs">
-      <template #item="{ item }">
-        <div v-if="item.label === 'Unlimited'">
-          <p>You have {{ attempts[mode] }} revives left.</p>
-          <div>
-            <NuxtImg
-              :src="`https://cdn.warframestat.us/img/${warframeToGuess[mode]?.imageName}`"
-              :alt="warframeToGuess[mode].name"
-              class="h-16"
-            />
-          </div>
-          <div class="flex flex-col capitalize md:-ml-[15%] md:w-[130%]">
-            <p class="justify-self-center">Warframe</p>
-            <UiTile
-              v-for="warframe of guessedItems[mode]"
-              :key="warframe.name"
-              :is-correct="warframe.name === warframeToGuess[mode].belongsTo"
-            >
-              {{ warframe.name }}
-            </UiTile>
-          </div>
-        </div>
-        <div v-if="item.label === 'Daily'">Coming Soon</div>
-      </template>
-    </UTabs>
-    <ClassicCombobox v-if="!isGameOver[mode]" />
-    <div v-else>
-      <p>Game Over!</p>
-      <p>The answer was {{ warframeToGuess[mode].name }}</p>
-      <p>{{ attempts[mode] > 0 ? "You Won" : "You Lost Sucka" }}</p>
-      <UButton @click="resetGame">New Game</UButton>
+  <div v-if="mode && itemToGuess[mode]" class="flex flex-col gap-4">
+    <UTabs v-model="selectedTab" :items="tabs"> </UTabs>
+    <div v-if="mode">
+      <RemainingGuesses />
+      <div>
+        <NuxtImg
+          :src="`https://cdn.warframestat.us/img/${itemToGuess[mode]?.imageName}`"
+          alt="Warframe to guess"
+          :class="[
+            'h-16',
+            {
+              'blur-sm': (attempts[mode] = 6),
+              invert: attempts[mode] >= 5,
+              'rotate-45': attempts[mode] >= 4,
+            },
+          ]"
+        />
+      </div>
+      <div class="flex flex-col capitalize md:-ml-[15%] md:w-[130%]">
+        <p class="justify-self-center">Warframe</p>
+        <UiTile
+          v-for="warframe of guessedItems[mode]"
+          :key="warframe.name"
+          :is-correct="warframe.name === itemToGuess[mode]?.belongsTo"
+        >
+          {{ warframe.name }}
+        </UiTile>
+      </div>
     </div>
+    <WarframeSearch
+      v-if="!isGameOver[mode]"
+      :items="vanillaWarframes"
+      comparison-field="belongsTo"
+    />
+    <GameOver />
   </div>
   <div v-else>
     <p>Loading...</p>
@@ -43,22 +45,19 @@ definePageMeta({
   layout: "game",
 });
 
-const { warframeToGuess, mode, guessedItems, attempts, isGameOver } =
-  storeToRefs(useGameStore());
-const { abilityInit, resetGame } = useGameStore();
+const {
+  itemToGuess,
+  mode,
+  guessedItems,
+  attempts,
+  isGameOver,
+  vanillaWarframes,
+} = storeToRefs(useGameStore());
+const { abilityInit } = useGameStore();
 
 await callOnce(abilityInit);
 
 // probably use zod to make sure that the data and all available
-
-watch(
-  () => attempts.value[mode.value],
-  () => {
-    if (attempts.value[mode.value] === 0) {
-      isGameOver.value[mode.value] = true;
-    }
-  },
-);
 
 // If I get the correct guess it should still be added to guessed items but then I need to update the game over condition
 

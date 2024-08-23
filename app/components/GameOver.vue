ff24dc742073c64609d0d4ff2eb2e64806f59878
kadeem-lewis <email@kadeemlewis.com>
2024-08-23 01:11:21 -0400

    updated game over to only show show guesses button for ability games

--- app/components/GameOver.vue
+++ app/components/GameOver.vue
@@ -1,27 +1,29 @@
 <template>
-  <div v-if="mode && isGameOver[mode]">
+  <div v-if="mode">
     <p>Game Over!</p>
     <p>The answer was {{ itemToGuess[mode]?.name }}</p>
     <p>{{ attempts[mode] > 0 ? "You Won" : "You Lost Sucka" }}</p>
-    <UButton variant="link" @click="showGuesses = !showGuesses"
-      >{{ showGuesses ? "Hide" : "Show" }} guesses</UButton
-    >
-    <ul v-if="showGuesses">
-      <li
-        v-for="guessedItem in guessedItems[mode]"
-        :key="guessedItem.name"
-        class="flex gap-2"
+    <div v-if="mode === 'ability' || mode === 'abilityUnlimited'">
+      <UButton variant="link" @click="showGuesses = !showGuesses"
+        >{{ showGuesses ? "Hide" : "Show" }} guesses</UButton
       >
-        <UIcon
-          :name="
-            isCorrectGuess(guessedItem.name)
-              ? 'heroicons:check-solid'
-              : 'heroicons:x-mark-solid'
-          "
-        />
-        {{ guessedItem.name }}
-      </li>
-    </ul>
+      <ul v-if="showGuesses">
+        <li
+          v-for="guessedItem in guessedItems[mode]"
+          :key="guessedItem.name"
+          class="flex gap-2"
+        >
+          <UIcon
+            :name="
+              isCorrectGuess(guessedItem.name)
+                ? 'heroicons:check-solid'
+                : 'heroicons:x-mark-solid'
+            "
+          />
+          {{ guessedItem.name }}
+        </li>
+      </ul>
+    </div>
     <UButton
       v-if="mode === 'abilityUnlimited' || mode === 'classicUnlimited'"
       @click="resetGame"
@@ -34,7 +36,7 @@
 </template>
 
 <script setup lang="ts">
-const { itemToGuess, mode, guessedItems, attempts, isGameOver } =
+const { itemToGuess, mode, guessedItems, attempts } =
   storeToRefs(useGameStore());
 const { resetGame } = useGameStore();
 
@@ -42,8 +44,10 @@ const showGuesses = ref(false);
 
 const isCorrectGuess = computed(() => (guess: string) => {
   if (!mode.value) return;
-  if (itemToGuess.value[mode.value]?.name === guess) {
-    return true;
+  if (mode.value === "ability" || mode.value === "abilityUnlimited") {
+    if (itemToGuess.value[mode.value]?.belongsTo === guess) {
+      return true;
+    }
   }
   return false;
 });

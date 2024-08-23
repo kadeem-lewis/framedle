1d65fc0de2f1c64c77d2aba446a01568db1fc7ce
kadeem-lewis <email@kadeemlewis.com>
2024-08-23 01:18:31 -0400

    updated routes to use new components and have different checks

--- app/pages/ability.vue
+++ app/pages/ability.vue
@@ -1,39 +1,16 @@
 <template>
-  <div v-if="mode && itemToGuess[mode]" class="flex flex-col gap-4">
-    <UTabs v-model="selectedTab" :items="tabs"> </UTabs>
-    <div v-if="mode">
+  <div v-if="mode" class="flex flex-col gap-4">
+    <ModeSwitch />
+    <ModeUnavailable v-if="!itemToGuess[mode]" />
+    <div v-else>
       <RemainingGuesses />
-      <div>
-        <NuxtImg
-          :src="`https://cdn.warframestat.us/img/${itemToGuess[mode]?.imageName}`"
-          alt="Warframe to guess"
-          :class="[
-            'h-16',
-            {
-              'blur-sm': (attempts[mode] = 6),
-              invert: attempts[mode] >= 5,
-              'rotate-45': attempts[mode] >= 4,
-            },
-          ]"
-        />
+      <AbilityImageToGuess />
+      <div v-if="!isGameOver[mode]">
+        <AbilityFeedbackArea />
+        <WarframeSearch :items="vanillaWarframes" />
       </div>
-      <div class="flex flex-col capitalize md:-ml-[15%] md:w-[130%]">
-        <p class="justify-self-center">Warframe</p>
-        <UiTile
-          v-for="warframe of guessedItems[mode]"
-          :key="warframe.name"
-          :is-correct="warframe.name === itemToGuess[mode]?.belongsTo"
-        >
-          {{ warframe.name }}
-        </UiTile>
-      </div>
+      <GameOver v-else />
     </div>
-    <WarframeSearch
-      v-if="!isGameOver[mode]"
-      :items="vanillaWarframes"
-      comparison-field="belongsTo"
-    />
-    <GameOver />
   </div>
   <div v-else>
     <p>Loading...</p>
@@ -45,42 +22,26 @@ definePageMeta({
   layout: "game",
 });
 
-const {
-  itemToGuess,
-  mode,
-  guessedItems,
-  attempts,
-  isGameOver,
-  vanillaWarframes,
-} = storeToRefs(useGameStore());
+const { itemToGuess, mode, isGameOver, vanillaWarframes } =
+  storeToRefs(useGameStore());
 const { abilityInit } = useGameStore();
 
 await callOnce(abilityInit);
 
-// probably use zod to make sure that the data and all available
-
 // If I get the correct guess it should still be added to guessed items but then I need to update the game over condition
 
-const router = useRouter();
 const route = useRoute();
 
-const tabs = [{ label: "Daily" }, { label: "Unlimited" }];
-
-const selectedTab = ref(route.query.mode ? 1 : 0);
-
 watch(
-  selectedTab,
-  (value) => {
-    if (tabs[value]?.label === "Unlimited") {
+  () => route.query.mode,
+  () => {
+    if (route.query.mode === "unlimited") {
       mode.value = "abilityUnlimited";
-      router.replace({ query: { mode: "unlimited" } });
-    } else if (tabs[value]?.label === "Daily") {
+    }
+    if (!route.query.mode) {
       mode.value = "ability";
-      router.replace(route.path);
     }
   },
-  {
-    immediate: true,
-  },
+  { immediate: true },
 );
 </script>
--- app/pages/classic.vue
+++ app/pages/classic.vue
@@ -1,43 +1,37 @@
 <template>
-  <div class="flex flex-col gap-4">
-    <UTabs v-model="selectedTab" :items="tabs">
-      <template #item="{ item }">
-        <div v-if="item.label === 'Unlimited'">
-          <RemainingGuesses />
-          <div
-            class="grid grid-cols-6 gap-2 capitalize md:-ml-[15%] md:w-[130%]"
-          >
-            <p
-              v-for="label of [
-                'name',
-                'sex',
-                'health',
-                'shield',
-                'progenitor',
-                'release year',
-              ]"
-              :key="label"
-              class="justify-self-center"
-            >
-              {{ label }}
-            </p>
-            <ClassicFeedbackRow
-              v-for="warframe of guessedItems[mode]"
-              :key="warframe.name"
-              :guessed-warframe="warframe"
-              :correct-warframe="itemToGuess[mode]"
-            />
-          </div>
-        </div>
-        <div v-if="item.label === 'Daily'">Coming Soon</div>
-      </template>
-    </UTabs>
-    <WarframeSearch
-      v-if="!isGameOver[mode]"
-      :items="warframes"
-      comparison-field="name"
-    />
-    <GameOver />
+  <div
+    v-if="mode && (mode === 'classic' || mode === 'classicUnlimited')"
+    class="flex flex-col gap-4"
+  >
+    <ModeSwitch />
+    <ModeUnavailable v-if="!itemToGuess[mode]" />
+    <div v-else>
+      <RemainingGuesses />
+      <div class="grid grid-cols-6 gap-2 capitalize md:-ml-[15%] md:w-[130%]">
+        <p
+          v-for="label of [
+            'name',
+            'sex',
+            'base health',
+            'base shield',
+            'progenitor',
+            'release year',
+          ]"
+          :key="label"
+          class="justify-self-center text-center"
+        >
+          {{ label }}
+        </p>
+        <ClassicFeedbackRow
+          v-for="warframe of guessedItems[mode]"
+          :key="warframe.name"
+          :guessed-warframe="warframe"
+          :correct-warframe="itemToGuess[mode]"
+        />
+      </div>
+      <WarframeSearch v-if="!isGameOver[mode]" :items="warframes" />
+      <GameOver v-else />
+    </div>
   </div>
 </template>
 
@@ -52,30 +46,20 @@ const { classicInit } = useGameStore();
 
 await callOnce(classicInit);
 
-// probably use zod to make sure that the data and all available
-
 // If I get the correct guess it should still be added to guessed items but then I need to update the game over condition
 
-const router = useRouter();
 const route = useRoute();
 
-const tabs = [{ label: "Daily" }, { label: "Unlimited" }];
-
-const selectedTab = ref(route.query.mode ? 1 : 0);
-
 watch(
-  selectedTab,
-  (value) => {
-    if (tabs[value]?.label === "Unlimited") {
-      mode.value = "classicUnlimited";
-      router.replace({ query: { mode: "unlimited" } });
-    } else if (tabs[value]?.label === "Daily") {
+  () => route.query.mode,
+  () => {
+    if (!route.query.mode) {
       mode.value = "classic";
-      router.replace(route.path);
+    }
+    if (route.query.mode === "unlimited") {
+      mode.value = "classicUnlimited";
     }
   },
-  {
-    immediate: true,
-  },
+  { immediate: true },
 );
 </script>

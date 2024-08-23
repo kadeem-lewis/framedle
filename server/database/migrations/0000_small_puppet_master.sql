aede70587eae0b47e1bdfe4c8a6fafdaae3b8634
kadeem-lewis <email@kadeemlewis.com>
2024-08-23 01:07:59 -0400

    reset db migrations

--- server/database/migrations/0000_small_puppet_master.sql
+++ server/database/migrations/0000_small_puppet_master.sql
@@ -2,8 +2,9 @@ CREATE TABLE `daily` (
 	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
 	`date` text NOT NULL,
 	`day` integer NOT NULL,
-	`classicId` integer NOT NULL,
-	`abilityId` integer NOT NULL
+	`classicId` text NOT NULL,
+	`abilityId` text NOT NULL
 );
 --> statement-breakpoint
+CREATE UNIQUE INDEX `daily_date_unique` ON `daily` (`date`);--> statement-breakpoint
 CREATE UNIQUE INDEX `daily_day_unique` ON `daily` (`day`);

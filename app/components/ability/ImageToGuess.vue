316da85f466a8d05634821ebf3c082bee7b22427
kadeem-lewis <email@kadeemlewis.com>
2024-08-23 02:17:12 -0400

    ability games now see description and name of ability as guesses get lower

--- app/components/ability/ImageToGuess.vue
+++ app/components/ability/ImageToGuess.vue
@@ -1,20 +1,28 @@
 <template>
-  <div v-if="mode" class="flex items-center justify-center p-4">
-    <NuxtImg
-      :src="`https://cdn.warframestat.us/img/${itemToGuess[mode]?.imageName}`"
-      alt="Warframe to guess"
-      :class="[
-        'h-32',
-        {
-          'blur-sm': attempts[mode] === 6,
-          invert: attempts[mode] >= 5,
-          'rotate-45': attempts[mode] >= 4,
-        },
-      ]"
-    />
+  <div v-if="mode && (mode === 'ability' || mode === 'abilityUnlimited')">
+    <div class="flex items-center justify-center p-4">
+      <NuxtImg
+        :src="`https://cdn.warframestat.us/img/${itemToGuess[mode]?.imageName}`"
+        alt="Warframe to guess"
+        :class="[
+          'h-32',
+          {
+            'blur-sm': attempts[mode] === 6 && !isGameOver[mode],
+            invert: attempts[mode] >= 5 && !isGameOver[mode],
+            'rotate-45': attempts[mode] >= 4 && !isGameOver[mode],
+          },
+        ]"
+      />
+    </div>
+    <p v-if="attempts[mode] <= 3 || isGameOver[mode]">
+      {{ itemToGuess[mode]?.description.split(".")[0] }}.
+    </p>
+    <p v-if="attempts[mode] <= 2 || isGameOver[mode]">
+      {{ itemToGuess[mode]?.name }}
+    </p>
   </div>
 </template>
 
 <script setup lang="ts">
-const { itemToGuess, mode, attempts } = storeToRefs(useGameStore());
+const { itemToGuess, mode, attempts, isGameOver } = storeToRefs(useGameStore());
 </script>

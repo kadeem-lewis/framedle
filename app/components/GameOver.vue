c016decee192b9104f738360eb08990bad7e54fb
kadeem-lewis <email@kadeemlewis.com>
2024-08-23 02:16:28 -0400

    added timer to game over and showing correct answer for game mode

--- app/components/GameOver.vue
+++ app/components/GameOver.vue
@@ -1,7 +1,7 @@
 <template>
   <div v-if="mode">
     <p>Game Over!</p>
-    <p>The answer was {{ itemToGuess[mode]?.name }}</p>
+    <p>The answer was {{ answer }}</p>
     <p>{{ attempts[mode] > 0 ? "You Won" : "You Lost Sucka" }}</p>
     <div v-if="mode === 'ability' || mode === 'abilityUnlimited'">
       <UButton variant="link" @click="showGuesses = !showGuesses"
@@ -29,13 +29,15 @@
       @click="resetGame"
       >New Game</UButton
     >
-    <div v-else>
+    <div v-else class="flex gap-2">
       <p>New Game in:</p>
+      <NextGameCountdown :target-date="startOfTomorrow()" />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { startOfTomorrow } from "date-fns";
 const { itemToGuess, mode, guessedItems, attempts } =
   storeToRefs(useGameStore());
 const { resetGame } = useGameStore();
@@ -50,5 +52,14 @@ const isCorrectGuess = computed(() => (guess: string) => {
     }
   }
   return false;
+});
+
+const answer = computed(() => {
+  if (!mode.value) return;
+  if (mode.value === "ability" || mode.value === "abilityUnlimited") {
+    return itemToGuess.value[mode.value]?.belongsTo;
+  } else {
+    return itemToGuess.value[mode.value]?.name;
+  }
 });
 </script>

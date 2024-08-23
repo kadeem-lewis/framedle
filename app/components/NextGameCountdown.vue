d13f0b01c8e764dff7dd4cd0ec0b0d2ec02f5883
kadeem-lewis <email@kadeemlewis.com>
2024-08-23 02:17:33 -0400

    created a timer to show time until next game

---
+++ app/components/NextGameCountdown.vue
@@ -0,0 +1,48 @@
+<template>
+  <div v-if="timeLeft <= 0">Time's up! Reload to see new Game.</div>
+  <div v-else>
+    <p>{{ formatTimeLeft(timeLeft) }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { differenceInSeconds } from "date-fns";
+
+const props = defineProps<{
+  targetDate: Date;
+}>();
+
+const timeLeft = ref();
+
+const intervalId = setInterval(() => {
+  const newTimeLeft = calculateTimeLeft(props.targetDate);
+
+  if (newTimeLeft <= 0) {
+    clearInterval(intervalId);
+  }
+
+  timeLeft.value = newTimeLeft;
+}, 1000);
+
+function calculateTimeLeft(endDate: Date) {
+  const now = new Date();
+  return differenceInSeconds(endDate, now);
+}
+
+function formatTimeLeft(timeLeftInSeconds: number) {
+  const hours = Math.floor(timeLeftInSeconds / 3600);
+  let remaining = timeLeftInSeconds % 3600;
+  const minutes = Math.floor(remaining / 60);
+  remaining = remaining % 60;
+  const seconds = remaining;
+  return `${hours.toString().padStart(2, "0")}:${minutes
+    .toString()
+    .padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+}
+
+tryOnBeforeUnmount(() => {
+  clearInterval(intervalId);
+});
+</script>
+
+<style scoped></style>

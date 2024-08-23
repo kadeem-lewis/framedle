8f716ae0441dea4bc165c8b63d67d71aed3d9f59
kadeem-lewis <email@kadeemlewis.com>
2024-08-23 01:10:07 -0400

    created a unique component for the image to guess

---
+++ app/components/ability/ImageToGuess.vue
@@ -0,0 +1,20 @@
+<template>
+  <div v-if="mode" class="flex items-center justify-center p-4">
+    <NuxtImg
+      :src="`https://cdn.warframestat.us/img/${itemToGuess[mode]?.imageName}`"
+      alt="Warframe to guess"
+      :class="[
+        'h-32',
+        {
+          'blur-sm': attempts[mode] === 6,
+          invert: attempts[mode] >= 5,
+          'rotate-45': attempts[mode] >= 4,
+        },
+      ]"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+const { itemToGuess, mode, attempts } = storeToRefs(useGameStore());
+</script>

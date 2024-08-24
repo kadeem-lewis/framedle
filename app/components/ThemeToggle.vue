ea012ed372a4a13c97b1d20a434f8eb2b1f87aa4
kadeem-lewis <email@kadeemlewis.com>
2024-08-24 01:59:29 -0400

    created a theme toggle component to change the theme of app

---
+++ app/components/ThemeToggle.vue
@@ -0,0 +1,54 @@
+<template>
+  <ColorScheme>
+    <UDropdown v-model="$colorMode.preference" :items="items">
+      <UButton
+        variant="outline"
+        square
+        :ui="{
+          rounded: false,
+        }"
+        class="border-primary border-b-4"
+      >
+        <UIcon
+          name="i-heroicons-sun-solid"
+          class="size-5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+        />
+        <UIcon
+          name="i-heroicons-moon-solid"
+          class="absolute size-5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+        />
+      </UButton>
+    </UDropdown>
+  </ColorScheme>
+</template>
+
+<script setup lang="ts">
+const colorMode = useColorMode();
+
+const items = [
+  [
+    {
+      label: "system",
+      click: () => {
+        colorMode.preference = "system";
+      },
+    },
+  ],
+  [
+    {
+      label: "light",
+      click: () => {
+        colorMode.preference = "light";
+      },
+    },
+  ],
+  [
+    {
+      label: "dark",
+      click: () => {
+        colorMode.preference = "dark";
+      },
+    },
+  ],
+];
+</script>

311fdd7e7416d3701816fa3221d1aa3443d356ab
kadeem-lewis <email@kadeemlewis.com>
2024-08-24 02:24:30 -0400

    added zod validation and proper error returning to dailies api route

--- server/api/dailies/index.get.ts
+++ server/api/dailies/index.get.ts
@@ -1,10 +1,23 @@
+import { z } from "zod";
+
+const dateSchema = z.object({
+  date: z.string().date(),
+});
+
 export default defineEventHandler<{ query: { date: string } }>(
   async (event) => {
-    const { date } = getQuery(event);
+    const result = await getValidatedQuery(event, (body) =>
+      dateSchema.safeParse(body),
+    );
 
-    if (!date) {
-      return { status: 400, body: { error: "Invalid date" } };
+    if (!result.success) {
+      throw createError({
+        statusCode: 400,
+        message: "Invalid date",
+      });
     }
+
+    const { date } = result.data;
     try {
       const result = await useDrizzle()
         .select()
@@ -13,7 +26,10 @@ export default defineEventHandler<{ query: { date: string } }>(
         .limit(1);
 
       if (result.length === 0) {
-        return { status: 404, body: { error: "Not found" } };
+        throw createError({
+          statusCode: 404,
+          message: "Entry not found for this date",
+        });
       }
       return {
         status: 200,
@@ -21,7 +37,10 @@ export default defineEventHandler<{ query: { date: string } }>(
       };
     } catch (error) {
       console.error(error);
-      return { status: 500, body: { error: "Internal server error" } };
+      throw createError({
+        statusCode: 500,
+        message: "Internal server error",
+      });
     }
   },
 );

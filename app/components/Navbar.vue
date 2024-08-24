b2a8de6bb6a794b386f656c5b643f0e2c82217cd
kadeem-lewis <email@kadeemlewis.com>
2024-08-24 02:06:31 -0400

    removed options from navbar and added theme toggle

--- app/components/Navbar.vue
+++ app/components/Navbar.vue
@@ -5,64 +5,23 @@
         <NuxtLink to="/">Framedle</NuxtLink>
       </h1>
     </div>
-    <div class="flex gap-1">
-      <UButton
-        icon="i-heroicons-calendar-solid"
-        variant="ghost"
-        @click="
-          isOpen = true;
-          selectedOption = options.ARCHIVE;
-        "
-      />
-      <UButton
-        icon="i-heroicons-chart-bar-solid"
-        variant="ghost"
-        @click="
-          isOpen = true;
-          selectedOption = options.STATS;
-        "
-      />
-      <UButton
-        icon="i-heroicons-information-circle"
-        variant="ghost"
-        @click="
-          isOpen = true;
-          selectedOption = options.ABOUT;
-        "
-      />
-      <UButton
-        icon="i-heroicons-question-mark-circle"
-        variant="ghost"
-        @click="
-          isOpen = true;
-          selectedOption = options.INSTRUCTIONS;
-        "
-      />
+    <div class="flex items-center gap-1">
       <UButton
         icon="i-heroicons-cog-6-tooth-solid"
-        variant="ghost"
-        @click="
-          isOpen = true;
-          selectedOption = options.SETTINGS;
-        "
+        variant="outline"
+        square
+        :ui="{
+          rounded: false,
+        }"
+        class="border-primary border-b-4"
+        @click="isOpen = true"
       />
+      <ThemeToggle />
     </div>
     <UModal v-model="isOpen"> hi </UModal>
   </header>
 </template>
 
 <script setup lang="ts">
-const options = {
-  ARCHIVE: "archive",
-  STATS: "stats",
-  ABOUT: "about",
-  INSTRUCTIONS: "instructions",
-  SETTINGS: "settings",
-} as const;
-
-type Option = (typeof options)[keyof typeof options];
-
-const selectedOption = ref<Option>();
-
 const isOpen = ref(false);
 </script>

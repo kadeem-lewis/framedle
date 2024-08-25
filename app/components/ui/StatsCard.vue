02175f951f4043de6703cb0b863d75fbe48924a3
kadeem-lewis <email@kadeemlewis.com>
2024-08-25 18:31:39 -0400

    created card to handle styling of stats fields

---
+++ app/components/ui/StatsCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <div
+    class="ring-primary flex flex-col items-center text-center font-semibold uppercase ring-1"
+  >
+    <p class="py-0.5">{{ props.value }}</p>
+    <p class="bg-primary w-full py-0.5">{{ props.label }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  label: string;
+  value: number | string;
+}>();
+</script>

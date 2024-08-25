0aeae8924f4da86cab20c2f823d08b74aa225416
kadeem-lewis <email@kadeemlewis.com>
2024-08-24 22:35:21 -0400

    changed drizzle to use turso as db provider

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'drizzle-kit'
+import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-  dialect: 'sqlite',
-  schema: './server/database/schema.ts',
-  out: './server/database/migrations'
-})
+  dialect: "sqlite",
+  schema: "./server/database/schema.ts",
+  out: "./server/database/migrations",
+  driver: "turso",
+  dbCredentials: {
+    url: process.env.TURSO_DATABASE_URL!,
+    authToken: process.env.TURSO_AUTH_TOKEN,
+  },
+});
--- server/utils/drizzle.ts
+++ server/utils/drizzle.ts
@@ -1,11 +1,21 @@
-import { drizzle } from "drizzle-orm/d1";
+import { drizzle } from "drizzle-orm/libsql";
 import * as schema from "../database/schema";
+import { createClient } from "@libsql/client";
 export { sql, eq, and, or } from "drizzle-orm";
+
+if (!process.env.TURSO_DATABASE_URL) {
+  throw new Error("TURSO_DATABASE_URL is not set");
+}
+
+export const db = createClient({
+  url: process.env.TURSO_DATABASE_URL,
+  authToken: process.env.TURSO_AUTH_TOKEN,
+});
 
 export const tables = schema;
 
 export function useDrizzle() {
-  return drizzle(hubDatabase(), { schema });
+  return drizzle(db, { schema });
 }
 
 export type Daily = typeof schema.daily.$inferSelect;

f5a2c98bc111d3b41d3ee8cc51263ab15e34a0fc
kadeem-lewis <email@kadeemlewis.com>
2024-08-26 02:22:49 -0400

    updated warframe api route to fetch from api instead of npm package

--- server/api/warframes.ts
+++ server/api/warframes.ts
@@ -1,11 +1,12 @@
-import Items from "warframe-items";
 import { progenitors } from "../constants/progenitors";
 import { warframeSchema } from "~~/schemas/warframe";
 import type { Warframe } from "~~/schemas/warframe";
 
-export default defineEventHandler(() => {
+export default defineEventHandler(async () => {
   const warframes: (Warframe | null)[] = [];
-  const data = new Items({ category: ["Warframes"] });
+  const data = await $fetch<Omit<Warframe, "progenitor">[]>(
+    "https://api.warframestat.us/warframes?remove=patchlogs",
+  );
   data.forEach((item) => {
     Object.assign(item, { progenitor: progenitors[item.name!] });
     const result = warframeSchema.safeParse(item);

d16806492d440de3c31c771493579a595ed8bfde
kadeem-lewis <email@kadeemlewis.com>
2024-08-25 21:50:31 -0400

    updated name of about to be multi word and began adding content to about

---
+++ app/components/content/AboutGame.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="space-y-4">
+    <div>
+      Framedle is a Warframe guessing game, inspired by
+      <ULink to="https://loldle.net/">Loldle</ULink>,<ULink
+        to="https://framed.wtf/"
+        >Framed</ULink
+      >, <ULink to="https://warmle.org/">Warmle</ULink> and similar games.
+    </div>
+    <div>Digital Extremes does not endorse or sponsor this project.</div>
+    <section class="space-y-4">
+      <p class="font-semibold">Special Thanks To:</p>
+      <ul class="list-disc space-y-2">
+        <li>
+          Warframe Community Developers for providing the data that powers this
+          app
+        </li>
+        <li>Pitchers for developing the font used in the Logo</li>
+        <li>wfdata.io for providing the warframe icons used in the app</li>
+      </ul>
+    </section>
+  </div>
+</template>
+
+<script setup lang="ts"></script>

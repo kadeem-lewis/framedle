014955208574d787fc11121a0a43310ef123d59b
kadeem-lewis <email@kadeemlewis.com>
2024-08-25 22:13:36 -0400

    updated index page main section to take up full container width

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -3,7 +3,7 @@
     class="flex min-h-screen flex-col items-center justify-between gap-y-2"
   >
     <Navbar />
-    <main class="grow">
+    <main class="w-full grow">
       <NuxtPage />
     </main>
     <Footer />

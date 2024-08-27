83bf41891736bf7ec63ac992e9eb943bab11a5ea
kadeem-lewis <email@kadeemlewis.com>
2024-08-27 03:17:16 -0400

    updated dailies api route to get entry before the specified date ordered in certain order

--- server/api/dailies.get.ts
+++ server/api/dailies.get.ts
@@ -1,6 +1,19 @@
-export default defineEventHandler(async () => {
+import { asc, desc, lte } from "drizzle-orm";
+
+export default defineEventHandler<{
+  query: { order: "desc" | "asc"; date: string };
+}>(async (event) => {
+  const { order = "asc", date } = getQuery(event);
+
+  const orderByClause =
+    order === "desc" ? desc(tables.daily.date) : asc(tables.daily.date);
+
   try {
-    const result = await useDrizzle().select().from(tables.daily);
+    const result = await useDrizzle()
+      .select()
+      .from(tables.daily)
+      .orderBy(orderByClause)
+      .where(lte(tables.daily.date, date));
 
     if (result.length === 0) {
       throw createError({

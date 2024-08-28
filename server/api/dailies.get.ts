6bea01451c8174e0abff5c5f1a4dc4be37e93633
kadeem-lewis <email@kadeemlewis.com>
2024-08-28 05:00:40 -0400

    updated dailies api route to return formatted date and to accept updated query params

--- server/api/dailies.get.ts
+++ server/api/dailies.get.ts
@@ -1,19 +1,20 @@
+import { format, parseISO } from "date-fns";
 import { asc, desc, lte } from "drizzle-orm";
 
 export default defineEventHandler<{
-  query: { order: "desc" | "asc"; date: string };
+  query: { order: "OLDEST" | "NEWEST"; date: string };
 }>(async (event) => {
-  const { order = "asc", date } = getQuery(event);
+  const { order = "NEWEST", date } = getQuery(event);
 
   const orderByClause =
-    order === "desc" ? desc(tables.daily.date) : asc(tables.daily.date);
+    order === "NEWEST" ? desc(tables.daily.date) : asc(tables.daily.date);
 
   try {
     const result = await useDrizzle()
       .select()
       .from(tables.daily)
-      .orderBy(orderByClause)
-      .where(lte(tables.daily.date, date));
+      .where(lte(tables.daily.date, date))
+      .orderBy(orderByClause);
 
     if (result.length === 0) {
       throw createError({
@@ -23,7 +24,10 @@ export default defineEventHandler<{
     }
     return {
       status: 200,
-      dailies: result,
+      dailies: result.map((daily) => ({
+        ...daily,
+        readableDate: format(parseISO(daily.date), "PPP"),
+      })),
     };
   } catch (error) {
     console.error(error);

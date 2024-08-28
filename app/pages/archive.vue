16b51b5114d64480b5422bfac5aa0d63a1c523d1
kadeem-lewis <email@kadeemlewis.com>
2024-08-28 05:01:25 -0400

    updated style of archive and added working filters and search

--- app/pages/archive.vue
+++ app/pages/archive.vue
@@ -1,27 +1,62 @@
 <template>
-  <div>
-    <p>Archive</p>
+  <div class="space-y-4">
+    <p class="text-xl font-bold uppercase">Archive</p>
     <div class="flex gap-2">
-      <UButton @click="selectedMode = 'classic'"> Classic </UButton>
-      <UButton @click="selectedMode = 'ability'"> Ability </UButton>
+      <UButton
+        :ui="{
+          rounded: false,
+        }"
+        variant="outline"
+        class="uppercase"
+        @click="selectedMode = 'classic'"
+      >
+        Classic
+      </UButton>
+      <UButton
+        :ui="{
+          rounded: false,
+        }"
+        variant="outline"
+        class="uppercase"
+        @click="selectedMode = 'ability'"
+      >
+        Ability
+      </UButton>
     </div>
-    <div class="flex gap-2">
-      <UButton @click="order = 'asc'"> Oldest </UButton>
-      <UButton @click="order = 'desc'"> Newest </UButton>
+    <div class="flex items-center justify-end gap-4">
+      <USelect
+        v-model="order"
+        :options="['OLDEST', 'NEWEST']"
+        :ui="{
+          rounded: false,
+        }"
+        class="uppercase"
+      >
+        <template #trailing>
+          <UIcon name="mdi:triangle-down" class="size-3" />
+        </template>
+      </USelect>
+      <UInput
+        v-model="searchQuery"
+        :ui="{
+          rounded: false,
+        }"
+        placeholder="SEARCH..."
+      />
     </div>
-    <div v-if="dailies" class="grid grid-cols-2 gap-4">
+    <div v-if="filteredDailies" class="grid grid-cols-2 gap-4">
       <p class="font-semibold">Name</p>
       <p class="font-semibold">Date</p>
       <div
-        v-for="daily in dailies"
+        v-for="daily of filteredDailies"
         :key="daily.id"
-        class="contents cursor-pointer"
+        class="contents cursor-pointer odd:bg-gray-700"
         @click="
           navigateTo({ path: `/${selectedMode}`, query: { date: daily.date } })
         "
       >
         <p>Framedle #{{ daily.day }}</p>
-        <p>{{ daily.date }}</p>
+        <p>{{ daily.readableDate }}</p>
         <UDivider class="col-span-2" />
       </div>
     </div>
@@ -30,11 +65,16 @@
 
 <script setup lang="ts">
 import { format, subDays } from "date-fns";
+import Fuse from "fuse.js";
+
+type UpdatedDaily = Daily & {
+  readableDate: string;
+};
 
 const route = useRoute();
 const router = useRouter();
 
-const order = ref<"asc" | "desc">("desc");
+const order = ref<"OLDEST" | "NEWEST">("NEWEST");
 const selectedMode = ref(route.query.mode || "classic");
 
 const { data } = await useFetch("/api/dailies", {
@@ -44,8 +84,6 @@ const { data } = await useFetch("/api/dailies", {
     date: format(subDays(new Date(), 1), "yyyy-MM-dd"),
   },
 });
-
-const dailies = ref(data.value.dailies);
 
 watch(
   () => selectedMode.value,
@@ -59,9 +97,30 @@ watch(
   { immediate: true },
 );
 
-// In pokedexle, archive worked by getting the first date and then limiting a calendar to only dates after that date. Then whenever someone clicked on a date, it would just refetch the data with that date. Here I am fetching every date already so that approach would be very redundant.
+const filteredDailies = ref<UpdatedDaily[]>(data.value.dailies);
+const searchQuery = ref("");
 
-// I don't need to store this in the store, I can set use fetch and have it react to different things like the filters and fetch again
+const fuse = new Fuse(data.value.dailies as UpdatedDaily[], {
+  keys: ["readableDate", "day"],
+  threshold: 0.4,
+});
+
+watch(data, (newData) => {
+  filteredDailies.value = newData.dailies;
+  fuse.setCollection(newData.dailies);
+});
+
+watch(searchQuery, (newQuery) => {
+  if (newQuery === "") {
+    filteredDailies.value = data.value.dailies;
+  } else {
+    filteredDailies.value = fuse
+      .search(newQuery)
+      .map((result) => ({ ...result.item }));
+  }
+});
+
+// In pokedexle, archive worked by getting the first date and then limiting a calendar to only dates after that date. Then whenever someone clicked on a date, it would just refetch the data with that date. Here I am fetching every date already so that approach would be very redundant.
 
 // when a user clicks on a game, then it will redirect them back to the page for whatever game mode they were on and add the date to the url as a query param. The game page will watch(?) this query param and first check to see if dailies exists and read from that, otherwise it will just make a fetch request with that date
 </script>

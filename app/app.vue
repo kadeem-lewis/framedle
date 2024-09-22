f6da59936c65ed927f5a85f30468c5f7b480de7a
kadeem-lewis <email@kadeemlewis.com>
2024-09-22 05:27:19 -0400

    created a parallax background for app

--- app/app.vue
+++ app/app.vue
@@ -8,10 +8,35 @@ useHead({
     },
   ],
 });
+
+const backgroundRef = useTemplateRef("background");
+const parallax = reactive(useParallax(backgroundRef));
 </script>
 
 <template>
-  <NuxtLayout>
-    <NuxtPage />
-  </NuxtLayout>
+  <div>
+    <div
+      ref="background"
+      class="background"
+      :style="{
+        backgroundPosition: `calc(50% + ${parallax.tilt * 30}px) calc(50% + ${parallax.roll * 30}px)`,
+      }"
+    />
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
 </template>
+<style scoped>
+.background {
+  background-image: url("/backgrounds/circuit-board.svg");
+  background-repeat: repeat;
+  background-size: 400px 400px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  min-width: 100vw;
+  min-height: 100vh;
+  z-index: -20;
+}
+</style>

14ffa135fa5b6254ca1f609d47db774e6587cef7
kadeem-lewis <email@kadeemlewis.com>
2024-10-11 05:37:19 -0400

    began updating button styles to better work in both light and dark mode

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,9 +1,15 @@
 export default defineAppConfig({
   ui: {
-    primary: "lime",
+    primary: "amber",
     gray: "neutral",
     container: {
       constrained: "max-w-md",
+    },
+    button: {
+      variant: {
+        outline:
+          "bg-white/75 dark:bg-gray-800/75 text-gray-800 dark:text-primary",
+      },
     },
   },
 });

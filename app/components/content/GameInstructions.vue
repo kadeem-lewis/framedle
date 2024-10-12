3734a40b807ee4e510aaba4b076a40c4c68c7e65
kadeem-lewis <email@kadeemlewis.com>
2024-10-12 01:09:37 -0400

    updated game instructions to include more detailed instructions

--- app/components/content/GameInstructions.vue
+++ app/components/content/GameInstructions.vue
@@ -1,4 +1,9 @@
 <template>
+  <p>Next Challenge in:</p>
+  <span class="flex items-center gap-1">
+    <UIcon name="i-mdi-circle-slice-2" class="size-5" />
+    <NextGameCountdown :target-date="startOfTomorrow()" />
+  </span>
   <div v-if="$route.name === 'classic'">
     <p>Guess the Warframe in {{ defaultAttempts }} tries</p>
     <p>
@@ -23,7 +28,33 @@
     </p>
     <UDivider />
     <p>Example</p>
+    <p>Consider the correct answer is Nezha</p>
+    <p>If you enter <span>Inaros</span>, these properties will appear:</p>
+    <div class="grid grid-cols-6 gap-1">
+      <ClassicFeedbackRow
+        :guessed-warframe="Inaros!"
+        :correct-warframe="Nezha!"
+      />
+    </div>
+    <p>Sex: <span>Green</span></p>
+    <p>It is a match because both are the same sex</p>
+    <p>Health: Red and a down arrow</p>
+    <p>Nezha has less health than Inaros</p>
+    <p>Shields: Red and an up arrow</p>
+    <p>Nezha has more shields than Inaros</p>
+    <p>Progenitor: Green</p>
+    <p>It is a match because both have the same progenitor element</p>
+    <p>Release Year: Red and a down arrow</p>
+    <p>Nezha was released before Inaros</p>
+    <p>If you entered Nezha, here is what would come up:</p>
+    <div class="grid grid-cols-6 gap-1">
+      <ClassicFeedbackRow
+        :guessed-warframe="Nezha!"
+        :correct-warframe="Nezha!"
+      />
+    </div>
   </div>
+
   <div v-if="$route.name === 'ability'" class="space-y-4">
     <p>
       Guess the Warframe the ability belongs to in {{ defaultAttempts }} tries
@@ -33,5 +64,11 @@
 </template>
 
 <script setup lang="ts">
+import { startOfTomorrow } from "date-fns";
 const { defaultAttempts } = useGameStore();
+
+const { warframes } = storeToRefs(useGameStore());
+
+const Inaros = warframes.value.find((warframe) => warframe.name === "Inaros");
+const Nezha = warframes.value.find((warframe) => warframe.name === "Nezha");
 </script>

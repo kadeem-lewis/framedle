c149940776bd3ca62c33c1a849b235573d7476be
kadeem-lewis <email@kadeemlewis.com>
2024-10-14 01:36:18 -0400

    created a component to represent cards for each game mode

---
+++ app/components/ModeCard.vue
@@ -0,0 +1,37 @@
+<template>
+  <UCard
+    class="border-primary group relative cursor-pointer border uppercase transition-all hover:bg-opacity-100"
+    @click="navigateTo(tab.route)"
+  >
+    <div
+      :style="{ backgroundImage: `url(${tab.background})` }"
+      class="absolute inset-0 z-0 bg-cover contrast-75 group-hover:contrast-100 dark:brightness-50 dark:contrast-100 group-hover:dark:brightness-75"
+    />
+    <div class="relative z-10 flex flex-row items-center gap-8 text-white">
+      <NuxtImg
+        format="webp"
+        :src="tab.source"
+        :alt="tab.label"
+        class="h-16 group-hover:scale-110"
+      />
+      <div class="brightness-90 group-hover:brightness-100">
+        <p class="text-xl font-bold">
+          {{ tab.label }}
+        </p>
+        <p class="font-semibold">{{ tab.description }}</p>
+      </div>
+    </div>
+  </UCard>
+</template>
+
+<script setup lang="ts">
+const { tab } = defineProps<{
+  tab: {
+    label: string;
+    route: string;
+    source: string;
+    background: string;
+    description: string;
+  };
+}>();
+</script>

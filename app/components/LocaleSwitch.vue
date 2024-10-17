3787d0eb11ca7c772534cfab828eba7fa53f71df
kadeem-lewis <email@kadeemlewis.com>
2024-10-17 11:15:05 -0400

    began working on creating a localeSwitch

---
+++ app/components/LocaleSwitch.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+const { locale, locales } = useI18n();
+const switchLocalePath = useSwitchLocalePath();
+
+console.log(locale.value, locales.value);
+
+const localeIconMap: { [key: string]: string } = {
+  en: "i-openmoji-flag-united-states",
+  es: "i-openmoji-flag-spain",
+  pt: "i-openmoji-flag-brazil",
+};
+
+const selectedLocale = ref(locale.value);
+</script>
+
+<template>
+  <USelectMenu
+    v-model="selectedLocale"
+    :options="locales"
+    :on-change="switchLocalePath"
+    option-attribute="name"
+    class="border-primary dark:border-primary hover:border-primary border border-b-4 border-gray-800 bg-gray-100/75 dark:bg-gray-800/75"
+  >
+    <template #label>
+      <UIcon :name="localeIconMap[selectedLocale]!" class="size-5" />
+    </template>
+    <template #option="{ option }">
+      <span class="flex w-fit items-center gap-1">
+        <UIcon :name="localeIconMap[option.code]!" />
+        {{ option.name }}
+      </span>
+    </template>
+  </USelectMenu>
+</template>
--- app/components/SiteNavbar.vue
+++ app/components/SiteNavbar.vue
@@ -6,32 +6,10 @@
       </h1>
     </div>
     <div class="flex items-center gap-1">
-      <UButton
-        aria-label="settings"
-        icon="i-heroicons-cog-6-tooth-solid"
-        variant="outline"
-        square
-        class="dark:border-primary hover:border-primary border-b-4 border-gray-800"
-        @click="isOpen = true"
-      />
+      <LocaleSwitch />
       <ThemeToggle />
     </div>
-    <UModal v-model="isOpen">
-      <UCard
-        :ui="{
-          ring: '',
-          divide: 'divide-y divide-gray-100 dark:divide-gray-800',
-        }"
-      >
-        <template #header>
-          <p class="text-center text-xl font-semibold uppercase">Settings</p>
-        </template>
-        <ContentGameSettings />
-      </UCard>
-    </UModal>
   </header>
 </template>
 
-<script setup lang="ts">
-const isOpen = ref(false);
-</script>
+<script setup lang="ts"></script>

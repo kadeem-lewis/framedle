f3c7899c4de10a81aebbaa6205baf96c4a7d95a6
kadeem-lewis <email@kadeemlewis.com>
2024-10-18 01:55:02 -0400

    removed openmoji in favor of using html codes for emojis

--- app/components/LocaleSwitch.vue
+++ app/components/LocaleSwitch.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
 const { locale, locales, setLocale } = useI18n();
 
-const localeIconMap: { [key: string]: string } = {
-  en: "i-openmoji-flag-united-states",
-  es: "i-openmoji-flag-spain",
-  pt: "i-openmoji-flag-brazil",
+const localeIconMap: { [key: string]: number[] } = {
+  en: [127482, 127480],
+  es: [127466, 127480],
+  pt: [127463, 127479],
 };
 
 const selectedLocale = ref(locale.value);
@@ -19,21 +19,22 @@ watch(selectedLocale, (newValue) => {
     v-model="selectedLocale"
     :options="locales"
     :ui-menu="{
-      width: 'w-fit',
+      width: 'w-max',
     }"
     option-attribute="name"
     value-attribute="code"
     class="border-primary dark:border-primary hover:border-primary border border-b-4 border-gray-800 bg-gray-100/75 dark:bg-gray-800/75"
   >
     <UButton
-      :icon="localeIconMap[selectedLocale]"
       :icon-only="true"
       variant="outline"
       :aria-label="`Switch to ${selectedLocale}`"
-    />
+    >
+      {{ String.fromCodePoint(...localeIconMap[selectedLocale]!) }}
+    </UButton>
     <template #option="{ option }">
       <span class="flex w-fit items-center gap-1">
-        <UIcon :name="localeIconMap[option.code]!" />
+        {{ String.fromCodePoint(...localeIconMap[option.code]!) }}
         {{ option.name }}
       </span>
     </template>

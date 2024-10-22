d13328f3afb77294b3938a4f4167a6248bd74d19
kadeem-lewis <email@kadeemlewis.com>
2024-10-21 23:44:34 -0400

    created custom ogImage for app

---
+++ app/components/ogImage/Framedle.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="flex h-full w-full items-center justify-center bg-gray-50">
+    <div class="flex h-full w-full flex-col items-center justify-center">
+      <h1 class="flex justify-center gap-4 text-[80px] font-black uppercase">
+        <span
+          v-for="(letter, index) in ['F', 'R', 'A', 'M', 'E', 'D', 'L', 'E']"
+          :key="index"
+          class="bg-green-500 px-5 py-2"
+        >
+          {{ letter }}
+        </span>
+      </h1>
+      <p class="mb-0 px-20 pb-10 text-4xl font-bold uppercase">
+        A Warframe Guessing Game
+      </p>
+    </div>
+  </div>
+</template>

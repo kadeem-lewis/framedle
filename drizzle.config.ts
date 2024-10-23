4a95fc667e9018513bd639f714cc3e64d60014ea
kadeem-lewis <email@kadeemlewis.com>
2024-10-23 02:50:06 -0400

    updated drizzle config to match updated structure

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-  dialect: "sqlite",
+  dialect: "turso",
   schema: "./server/database/schema.ts",
   out: "./server/database/migrations",
-  driver: "turso",
   dbCredentials: {
     url: process.env.TURSO_DATABASE_URL!,
     authToken: process.env.TURSO_AUTH_TOKEN,

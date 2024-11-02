91a1a897d48cc9ca707c3ec7cc7c2fa2ea15da3f
kadeem-lewis <email@kadeemlewis.com>
2024-11-01 22:07:58 -0400

    created composable to handle setting and updating game mode

---
+++ app/composables/useGameMode.ts
@@ -0,0 +1,20 @@
+type gameMode = "classic" | "classicUnlimited" | "ability" | "abilityUnlimited";
+
+export function useGameMode() {
+  const mode = ref<gameMode>();
+  const route = useRoute();
+
+  watch(
+    () => route.query.mode,
+    () => {
+      if (!route.query.mode) {
+        mode.value = route.name as gameMode;
+      }
+      if (route.query.mode === "unlimited") {
+        mode.value = `${route.name}Unlimited` as gameMode;
+      }
+    },
+    { immediate: true },
+  );
+  return mode;
+}

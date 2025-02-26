9d0d42e32b768aca2d9357e4060a5189316d0256
kadeem-lewis <email@kadeemlewis.com>
2025-02-25 21:40:15 -0500

    dark certain gray colors to allow better contrast with background

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="space-y-4">
     <p
-      class="font-roboto border border-gray-200 bg-gray-100/75 p-1 text-center text-lg font-semibold uppercase md:text-xl dark:border-gray-600/75 dark:bg-gray-800"
+      class="border border-gray-300 bg-gray-200/75 p-1 text-center font-roboto text-lg font-semibold uppercase md:text-xl dark:border-gray-600/75 dark:bg-gray-800"
     >
       Test your Warframe Knowledge!
     </p>

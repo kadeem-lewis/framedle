6b109dfda9c906f7644c3da216fcc925391b0626
kadeem-lewis <email@kadeemlewis.com>
2025-02-26 22:11:06 -0500

    readded settings button to navbar and added button border

--- app/components/SiteNavbar.vue
+++ app/components/SiteNavbar.vue
@@ -1,5 +1,7 @@
 <template>
-  <header class="flex w-full items-center justify-between pt-2">
+  <header
+    class="flex w-full items-center justify-between border-b border-gray-500 py-2 dark:border-gray-700"
+  >
     <div>
       <h1
         class="text-3xl font-bold uppercase tracking-widest [font-stretch:condensed]"
@@ -8,7 +10,38 @@
       </h1>
     </div>
     <div class="flex items-center gap-1">
+      <UButton
+        aria-label="settings"
+        icon="i-heroicons-cog-6-tooth-solid"
+        variant="outline"
+        square
+        class="dark:border-primary hover:border-primary border-b-4 border-gray-800"
+        :ui="{
+          icon: {
+            base: 'flex-shrink-0 transition-transform hover:-rotate-45',
+          },
+        }"
+        @click="isOpen = true"
+      />
       <ThemeToggle />
     </div>
+    <UModal v-model="isOpen">
+      <UCard
+        :ui="{
+          ring: '',
+          divide: 'divide-y divide-gray-100 dark:divide-gray-800',
+        }"
+      >
+        <template #header>
+          <p class="text-center font-roboto text-xl font-semibold uppercase">
+            Settings
+          </p>
+        </template>
+        <ContentGameSettings />
+      </UCard>
+    </UModal>
   </header>
 </template>
+<script setup lang="ts">
+const isOpen = ref(false);
+</script>

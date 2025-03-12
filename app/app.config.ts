3ac7bee549dcb39003197eedb9ef090332c84d66
kadeem-lewis <email@kadeemlewis.com>
2025-03-12 04:26:51 -0400

    moved app colors into dedicated  color block

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,14 +1,16 @@
 export default defineAppConfig({
   ui: {
-    primary: "amber",
-    gray: "slate",
+    colors: {
+      primary: "amber",
+      neutral: "slate",
+    },
     container: {
       constrained: "max-w-md",
     },
     button: {
       variant: {
         outline:
-          "bg-white/75 dark:bg-neutral-900/75 hover:text-primary text-black dark:text-primary rounded-none",
+          "bg-white/75 dark:bg-neutral-900/75 hover:text-(--ui-primary) text-black dark:text-(--ui-primary) rounded-none",
       },
     },
     card: {

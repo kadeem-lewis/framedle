3b81e53bc5fcdf8993575a78b409be4470d32b3b
kadeem-lewis <email@kadeemlewis.com>
2025-03-13 05:35:09 -0400

    updated logic of theme toggle to properly work

--- app/components/ThemeToggle.vue
+++ app/components/ThemeToggle.vue
@@ -1,60 +1,62 @@
 <template>
-  <ColorScheme>
-    <USelectMenu
-      v-model="$colorMode.preference"
-      :items="items"
-      selected-icon=""
-      option-attribute="name"
-      value-attribute="value"
-      :ui-menu="{
-        width: 'w-[400%]',
-        padding: 'p-1',
-        rounded: 'rounded-none',
-        option: {
-          rounded: 'rounded-none',
-          size: 'text-base',
-        },
-      }"
+  <UDropdownMenu
+    :items="items"
+    selected-icon=""
+    option-attribute="name"
+    value-attribute="value"
+    :ui="{
+      content: 'rounded-none hover:rounded-none',
+    }"
+  >
+    <UButton
+      variant="outline"
+      aria-label="theme-toggle"
+      square
+      class="border-b-4 border-(--ui-primary) bg-white/75 hover:border-(--ui-primary) dark:border-(--ui-primary) dark:bg-neutral-900/75"
     >
-      <UButton
-        variant="outline"
-        aria-label="theme-toggle"
-        square
-        class="border-b-4 border-(--ui-primary) border-neutral-800 bg-white/75 hover:border-(--ui-primary) dark:border-(--ui-primary) dark:bg-neutral-900/75"
-      >
-        <UIcon
-          name="i-heroicons-sun-solid"
-          class="size-5 scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90"
-        />
-        <UIcon
-          name="i-heroicons-moon-solid"
-          class="absolute size-5 scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0"
-        />
-      </UButton>
-      <template #option="{ option }">
-        <UIcon :name="option.icon" class="size-5" />
-        <span class="ml-2">{{ option.name }}</span>
-      </template>
-    </USelectMenu>
-  </ColorScheme>
+      <UIcon
+        name="i-heroicons-sun-solid"
+        class="size-5 scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90"
+      />
+      <UIcon
+        name="i-heroicons-moon-solid"
+        class="absolute size-5 scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0"
+      />
+    </UButton>
+    <template #item="{ item }">
+      <UIcon :name="item.icon" class="size-5" />
+      <span class="ml-2">{{ item.name }}</span>
+    </template>
+  </UDropdownMenu>
 </template>
 
 <script setup lang="ts">
+const colorMode = useColorMode();
+
 const items = [
   {
     name: "light",
     value: "light",
     icon: "i-heroicons-sun-solid",
+    onSelect: () => {
+      colorMode.preference = "light";
+    },
   },
   {
     name: "dark",
     value: "dark",
     icon: "i-heroicons-moon-solid",
+    onSelect: () => {
+      colorMode.preference = "dark";
+    },
   },
   {
     name: "system",
     value: "system",
     icon: "i-heroicons-computer-desktop",
+    onSelect: () => {
+      colorMode.preference = "system";
+    },
   },
 ];
 </script>

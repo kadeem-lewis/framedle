9ac27aedae8309abd1d9ebf553ed1ae2ddc830af
kadeem-lewis <email@kadeemlewis.com>
2025-03-28 16:58:22 -0400

    updated capacitor config to point to proper build

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,9 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'com.framedle',
-  appName: 'framedle',
-  webDir: 'www'
+  appId: "com.framedle",
+  appName: "framedle",
+  webDir: "dist",
 };
 
 export default config;

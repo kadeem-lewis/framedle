37a86bcc8492ebfe7aa85401963e527469008c10
kadeem-lewis <email@kadeemlewis.com>
2025-03-28 16:39:13 -0400

    added capacitor to project

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.framedle',
+  appName: 'framedle',
+  webDir: 'www'
+};
+
+export default config;

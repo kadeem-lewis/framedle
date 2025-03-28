709f3b5d86ad7e24be91d3f591555980c15e3a45
kadeem-lewis <email@kadeemlewis.com>
2025-03-28 17:22:02 -0400

    updated config to point to non symlinked version of files

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from "@capacitor/cli";
 const config: CapacitorConfig = {
   appId: "com.framedle",
   appName: "framedle",
-  webDir: "dist",
+  webDir: ".output/public",
 };
 
 export default config;

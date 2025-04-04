dc9decb4040fd872cbb9f5582f79b81705ad2f0f
kadeem-lewis <email@kadeemlewis.com>
2025-04-04 00:00:17 -0400

    began updating gameState to have greater functionality

--- app/composables/useGameState.ts
+++ app/composables/useGameState.ts
@@ -1,7 +1,75 @@
+import { useStorage } from "@vueuse/core";
+import type { GameMode } from "@/composables/useGameMode";
+
+export const GameStatus = {
+  ACTIVE: "active",
+  WON: "won",
+  LOST: "lost",
+  WON_PREVIOUS: "won:previous",
+} as const;
+
+export type GameStatusType = (typeof GameStatus)[keyof typeof GameStatus];
+
 export default function useGameState() {
   const { itemToGuess, guessedItems, attempts } = storeToRefs(useGameStore());
 
   const mode = useGameMode();
+
+  const gameState = useStorage<Record<GameMode, GameStatusType>>("gameState", {
+    classic: GameStatus.ACTIVE,
+    classicUnlimited: GameStatus.ACTIVE,
+    ability: GameStatus.ACTIVE,
+    abilityUnlimited: GameStatus.ACTIVE,
+  });
+
+  let won = false;
+  function updateGameState() {
+    // This logic is all types of wrong but it's a start.
+    const gameMode = mode.value;
+    if (!gameMode) throw createError("Mode is not set");
+
+    const currentGuessedItems = guessedItems.value[gameMode];
+    const currentAttempts = attempts.value[gameMode];
+
+    if (gameMode === "classic" || gameMode === "classicUnlimited") {
+      won = currentGuessedItems.some(
+        (guessedItem) => guessedItem.name === itemToGuess.value[gameMode]?.name,
+      );
+    }
+    if (gameMode === "ability" || gameMode === "abilityUnlimited") {
+      won = currentGuessedItems.some(
+        (guessedItem) =>
+          guessedItem.name === itemToGuess.value[gameMode]?.belongsTo,
+      );
+    }
+    if (won) {
+      //! When navigating between modes, it is constantly resetting games between won and won:previous.
+      if (
+        gameState.value[gameMode] !== GameStatus.WON &&
+        gameState.value[gameMode] !== GameStatus.WON_PREVIOUS
+      ) {
+        gameState.value[gameMode] = GameStatus.WON;
+      } else if (gameState.value[gameMode] === GameStatus.WON) {
+        gameState.value[gameMode] = GameStatus.WON_PREVIOUS;
+      }
+      return;
+    }
+    if (currentAttempts === 0) {
+      gameState.value[gameMode] = GameStatus.LOST;
+      return;
+    }
+
+    gameState.value[gameMode] = GameStatus.ACTIVE;
+  }
+
+  watch(
+    () => [mode.value, attempts.value],
+    () => {
+      updateGameState();
+      console.log(gameState.value);
+    },
+    { immediate: true, deep: true },
+  );
 
   const isGameOver = computed(() => ({
     classic:
@@ -53,5 +121,10 @@ export default function useGameState() {
     return false;
   });
 
-  return { hasWon, isGameOver };
+  function resetGameState(mode: MaybeRef<keyof typeof gameState.value>) {
+    const gameMode = toValue(mode);
+    gameState.value[gameMode] = GameStatus.ACTIVE;
+  }
+
+  return { hasWon, isGameOver, resetGameState };
 }

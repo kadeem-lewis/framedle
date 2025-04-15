b989f8727046fd2aa75ce5fcc635ffd73b10524b
Kadeem Lewis <71411720+kadeem-lewis@users.noreply.github.com>
2025-04-15 06:26:19 -0400

    Sentry setup (#51)
    * ran sentry setup wizard
    
    * updated sentry config to point to new project
    
    * updated start command to start sentry
    
    * reverted sentry config from using runtime config
    
    * fixed typo in start command

---
+++ sentry.client.config.ts
@@ -0,0 +1,23 @@
+import * as Sentry from "@sentry/nuxt";
+
+Sentry.init({
+  dsn: "https://fb7ff6ba8a0cda589d2881b274a8743b@o4508153642876928.ingest.us.sentry.io/4509149554606080",
+
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0,
+
+  // This sets the sample rate to be 10%. You may want this to be 100% while
+  // in development and sample at a lower rate in production
+  replaysSessionSampleRate: 0.1,
+
+  // If the entire session is not sampled, use the below sample rate to sample
+  // sessions when an error occurs.
+  replaysOnErrorSampleRate: 1.0,
+
+  // If you don't want to use Session Replay, just remove the line below:
+  integrations: [Sentry.replayIntegration()],
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});

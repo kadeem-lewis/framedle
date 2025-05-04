df002b88dee986a3970c650fde12b099a07efac5
kadeem-lewis <email@kadeemlewis.com>
2025-05-03 20:44:41 -0400

    updated game instructions for ability mode

--- app/components/content/GameInstructions.vue
+++ app/components/content/GameInstructions.vue
@@ -118,5 +118,10 @@ const error = computed(() => ($colorblindMode.value ? "Orange" : "Red"));
       Guess the Warframe the ability belongs to in {{ defaultAttempts }} tries
     </p>
     <p>{{ t("instructions.ability.subtitle") }}</p>
+    <p>
+      Images begin rotated and are placed in the correct orientation as
+      additional tiles are revealed
+    </p>
+    <p>The order in which tiles are revealed may change between days</p>
   </div>
 </template>

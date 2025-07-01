11cd8754599da1208cfe5a53028b41dfa455c9fe
kadeem-lewis <email@kadeemlewis.com>
2025-07-01 02:12:14 -0400

    updated check in layout for calling get daily

--- app/layouts/game.vue
+++ app/layouts/game.vue
@@ -6,7 +6,10 @@ watch(
   () => route.params,
   () => {
     if (route.name === "ability-path" || route.name === "classic-path") {
-      if (!route.params.path || validateParamAsDate(route.params.path.at(-1))) {
+      if (
+        !route.params.path ||
+        Number.isInteger(Number(route.params.path.at(-1)))
+      ) {
         getDaily();
       }
     }

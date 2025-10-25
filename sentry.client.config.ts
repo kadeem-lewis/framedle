11d8a9777d24c31a00c0309dd8b50b1311605563
kadeem-lewis <email@kadeemlewis.com>
2025-10-25 04:30:20 -0400

    updated sentry to not send logs in development

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -2,6 +2,7 @@ import * as Sentry from "@sentry/nuxt";
 
 Sentry.init({
   dsn: "https://fb7ff6ba8a0cda589d2881b274a8743b@o4508153642876928.ingest.us.sentry.io/4509149554606080",
+  enabled: process.env.NODE_ENV === "production",
 
   // We recommend adjusting this value in production, or using tracesSampler
   // for finer control

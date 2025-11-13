5ba8cc735e746d2af48904700abf45aedbeb1812
kadeem-lewis <email@kadeemlewis.com>
2025-11-13 07:07:41 -0500

    began generating a category list for different grid categories

---
+++ server/config/categories.ts
@@ -0,0 +1,73 @@
+export const categoryConfig = [
+  {
+    key: "sex",
+    type: "string",
+    template: (val: string) => `Warframes that are ${val}`,
+  },
+  {
+    key: "releaseDate",
+    type: "string",
+    template: (val: string) => `Warframes released in ${parseReleaseDate(val)}`,
+  },
+  {
+    key: "progenitor",
+    type: "string",
+    template: (val: string) => `Warframes with ${val} progenitor element`,
+  },
+  {
+    key: "isPrime",
+    type: "boolean",
+    template: (val: boolean) =>
+      val ? "Prime Warframes" : "Non-Prime Warframes",
+  },
+  {
+    key: "aura",
+    type: "string",
+    template: (val: string) => `Warframes with ${val} aura polarity`,
+  },
+  {
+    key: "playstyle",
+    type: "string",
+    template: (val: string) => `Warframes suited for ${val} playstyle`,
+  },
+  {
+    key: "conclave",
+    type: "boolean",
+    template: (val: boolean) =>
+      val ? "Conclave enabled Warframes" : "Non-Conclave Warframes",
+  },
+  {
+    key: "exalted",
+    type: "array",
+    template: (val: string[]) =>
+      val.length > 0
+        ? `Warframes with exalted weapons`
+        : "Warframes without exalted weapons",
+  },
+  {
+    key: "health",
+    type: "numeric_top_2",
+    template: (val: number) => `Warframes with ${val} base health`,
+  },
+  {
+    key: "shield",
+    type: "numeric_top_2",
+    template: (val: number) => `Warframes with ${val} base shield`,
+  },
+  {
+    key: "armor",
+    type: "numeric_top_2",
+    template: (val: number) => `Warframes with ${val} base armor`,
+  },
+  {
+    key: "energy",
+    type: "numeric_top_2",
+    template: (val: number) => `Warframes with ${val} base energy`,
+  },
+  {
+    key: "sprint",
+    type: "numeric_top_2",
+    template: (val: number) => `Warframes with ${val} sprint speed`,
+  },
+];
+// I want to eventually add protoframe, vaulted and maybe leverian

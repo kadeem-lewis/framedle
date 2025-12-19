b37995666bb6af117c491f7e9c41199403e9ffed
kadeem-lewis <email@kadeemlewis.com>
2025-12-19 06:53:16 -0500

    added placeholder seo to grid unlimited

--- app/pages/grid/unlimited.vue
+++ app/pages/grid/unlimited.vue
@@ -3,6 +3,12 @@ definePageMeta({
   layout: "game",
 });
 
+useSeoMeta({
+  title: "Grid Unlimited",
+  description:
+    "Play the unlimited grid mode of Framedle, where you can guess warframes on a grid with no restrictions.",
+});
+
 const { initializeUnlimitedGridGame } = useGridGameStore();
 const { unlimited, isLoading } = storeToRefs(useGridGameStore());
 

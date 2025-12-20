7032de237f225f1233143ede2b34156a9e1375da
kadeem-lewis <email@kadeemlewis.com>
2025-12-20 06:14:30 -0500

    created label component for grid

---
+++ app/components/grid/Label.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const { category } = defineProps<{
+  category: CategoryItem;
+}>();
+</script>
+<template>
+  <UPopover>
+    <UButton variant="ghost">{{ category.label }}</UButton>
+    <template #content>
+      <p class="p-2">{{ category.description }}</p>
+    </template>
+  </UPopover>
+</template>

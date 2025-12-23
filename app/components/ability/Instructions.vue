27082cb40258a293e485858abd241627ea70f4e5
kadeem-lewis <email@kadeemlewis.com>
2025-12-23 04:37:51 -0500

    split instructions into components for each mode

---
+++ app/components/ability/Instructions.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const { t } = useI18n();
+
+const { DEFAULT_ATTEMPTS } = useGameStore();
+</script>
+<template>
+  <div class="space-y-4">
+    <p>
+      Guess the Warframe the ability belongs to in {{ DEFAULT_ATTEMPTS }} tries
+    </p>
+    <p>{{ t("instructions.ability.subtitle") }}</p>
+    <p>
+      Images begin rotated and are placed in the correct orientation as
+      additional tiles are revealed
+    </p>
+    <p>The order in which tiles are revealed may change between days</p>
+  </div>
+</template>
---
+++ app/components/grid/Instructions.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts"></script>
+<template>
+  <div>
+    <p>Just guess stuff or something</p>
+  </div>
+</template>

4953d74c811aaae907eed70926e8d783bba477e8
kadeem-lewis <email@kadeemlewis.com>
2026-01-02 02:15:57 -0500

    updated cell min height

--- app/components/grid/Cell.vue
+++ app/components/grid/Cell.vue
@@ -15,7 +15,7 @@ const { isDaily } = useGameMode();
 </script>
 <template>
   <div
-    class="bg-elevated border-accented min-h-26 border-dashed p-1"
+    class="bg-elevated border-accented min-h-28 border-dashed p-1"
     :class="{
       'cursor-not-allowed': warframe || isGameOver,
       'hover:brightness-90 dark:hover:brightness-125': !warframe && !isGameOver,
@@ -35,8 +35,8 @@ const { isDaily } = useGameMode();
         :src="`https://cdn.warframestat.us/img/${warframe.imageName}`"
         :alt="`Image of ${warframe.name}`"
         format="avif"
-        height="96"
-        class="object-cover object-top pt-2"
+        height="108"
+        class="h-27 object-cover object-top pt-3"
       />
       <UBadge
         :label="warframe.name"

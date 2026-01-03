ee39d28f0806e89b80baa0321ed850e0e0b5b823
kadeem-lewis <email@kadeemlewis.com>
2026-01-03 04:00:02 -0500

    updated display of puzzle stats

--- app/components/grid/PuzzleStats.vue
+++ app/components/grid/PuzzleStats.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { format } from "date-fns";
+import type { TabsItem } from "@nuxt/ui";
 
 const { isGameOver } = storeToRefs(useGameStateStore());
 const { activeDays } = storeToRefs(useDailiesStore());
@@ -20,13 +21,22 @@ const { data } = useFetch("/api/stats", {
   key: `puzzle-stats-${statsQuery.value.date}`,
   lazy: true,
 });
+
+const items = ref<TabsItem[]>([
+  {
+    label: "Most Popular",
+  },
+  {
+    label: "Least Popular",
+  },
+]);
 </script>
 <template>
   <section class="flex flex-col items-center gap-4">
     <h2 class="text-xl font-semibold uppercase">Puzzle Stats</h2>
-    <div v-if="isGameOver">
+    <div class="flex flex-col gap-4">
       <UCard class="w-full">
-        <div class="flex justify-around">
+        <div class="flex w-full items-center justify-around gap-4">
           <div class="flex flex-col items-center justify-center">
             <span class="font-semibold uppercase">Games</span>
             <span>{{ data?.grid.gamesPlayed }}</span>
@@ -35,9 +45,16 @@ const { data } = useFetch("/api/stats", {
             <span class="font-semibold uppercase">Average Score</span>
             <span>{{ data?.grid.averageScore }}</span>
           </div>
+          <div class="flex flex-col items-center justify-center">
+            <span class="font-semibold uppercase">Most Unique</span>
+            <span>{{ data?.grid.mostUnique }}</span>
+          </div>
         </div>
       </UCard>
+      <div v-if="isGameOver">
+        <UTabs :items="items" :content="false" />
+      </div>
+      <div v-else>Complete game to see stats.</div>
     </div>
-    <div v-else>Complete game to see stats.</div>
   </section>
 </template>

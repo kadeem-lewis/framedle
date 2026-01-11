226599931f76f455223054c3483252a7652042e5
kadeem-lewis <email@kadeemlewis.com>
2026-01-11 07:37:14 -0500

    updated stats card to handle null values

--- app/components/ui/StatsCard.vue
+++ app/components/ui/StatsCard.vue
@@ -1,14 +1,14 @@
 <script setup lang="ts">
 const props = defineProps<{
   label: string;
-  value: number | string;
+  value: number | string | null;
 }>();
 </script>
 <template>
   <div
     class="ring-primary flex flex-col items-center text-center font-semibold uppercase ring-1"
   >
-    <p class="py-0.5">{{ props.value }}</p>
+    <p class="py-0.5">{{ props.value ?? "—" }}</p>
     <p class="bg-primary w-full py-0.5">{{ props.label }}</p>
   </div>
 </template>

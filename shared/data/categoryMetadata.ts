7ffba02b67761f641afa1216f50ec13983015160
kadeem-lewis <email@kadeemlewis.com>
2026-01-13 03:41:47 -0500

    updated category metadata to have better descriptors and to also have a function to safely query data

--- shared/data/categoryMetadata.ts
+++ shared/data/categoryMetadata.ts
@@ -1,68 +1,77 @@
+export type CategoryMetadata = {
+  header: string;
+  value: string | number;
+  description: string;
+  imgSrc?: string;
+  cssClass?: string;
+  extra?: string[];
+};
+
 export const categoryMetadata = {
   health: (value: number) => ({
-    category: "Base Health",
+    header: "Base Health",
     value,
     description: `Warframes with ${value} health at rank 0`,
     cssClass: "",
   }),
   shield: (value: number) => ({
-    category: "Base Shields",
+    header: "Base Shields",
     value,
     description: `Warframes with ${value} shields at rank 0`,
     cssClass: "",
   }),
   armor: (value: number) => ({
-    category: "Armor",
+    header: "Armor",
     value,
     description: `Warframes with ${value} armor`,
   }),
   energy: (value: number) => ({
-    category: "Base Energy",
+    header: "Base Energy",
     value,
     description: `Warframes with ${value} energy at rank 0`,
   }),
   sprint: (value: number) => ({
-    category: "Sprint Speed",
+    header: "Sprint Speed",
     value,
     description: `Warframes with ${value} sprint speed`,
   }),
   progenitor: (value: string) => ({
-    category: "Progenitor",
+    header: "Progenitor",
     value,
     description: `Warframes with ${value} progenitor element`,
     imgSrc: `/elements/${value.toLowerCase()}.png`,
   }),
   aura: (value: string) => ({
-    category: "Aura",
+    header: "Aura",
     value,
     description: `Warframes with ${value} aura polarity`,
     imgSrc: `/polarities/${value.toLowerCase()}.png`,
   }),
   playstyle: (value: string) => ({
-    category: "Playstyle",
+    header: "Playstyle",
     value,
     description: `Warframes with ${value} playstyle`,
     imgSrc: `/playstyles/${value.toLowerCase()}.png`,
   }),
   releaseDate: (value: string) => ({
-    category: "Release Year",
+    header: "Release Year",
     value,
     description: `Warframes released in ${value}`,
   }),
   sex: (value: string) => ({
-    category: "Sex",
+    header: "Sex",
     value,
     description: `Warframes that are ${value}`,
   }),
   isPrime: (value: boolean) => ({
-    category: "Variant",
+    header: "Variant",
     value: value ? "Prime" : "Original",
     description: value
       ? "Prime Warframes"
       : "Original Warframes ( Standard/Non-Prime )",
   }),
   conclave: (value: boolean) => ({
-    category: "Conclave",
+    header: "Conclave",
     value: value ? "Allowed" : "Disabled",
     description: value
       ? "Warframes that can be used in Conclave"
@@ -71,50 +80,59 @@ export const categoryMetadata = {
       "Lunaro and Faceoff are not considered Conclave modes for this category.",
     ],
   }),
-  vaulted: (value: boolean) => ({
-    // I don't want to count non vaulted since it changes too often
+  vaulted: (_: boolean) => ({
     //TODO: Label needs to be updated
-    category: "Vaulted",
-    value: value ? "Vaulted" : "Non-Vaulted",
-    description: value
-      ? "Warframes that are vaulted"
-      : "Warframes that are not vaulted",
+    header: "Vaulted",
+    value: "Vaulted",
+    description: "Warframes that have entered the Prime Vault",
   }),
-  exalted: (value: boolean) => ({
+  exalted: (_: boolean) => ({
     // non exalted is just too broad
     //TODO: Label needs to be updated
-    category: "Exalted Weapon",
-    value: value ? "Has Exalted Weapon" : "No Exalted Weapon",
-    description: value
-      ? "Warframes that have exalted weapons"
-      : "Warframes that do not have exalted weapons",
+    header: "Exalted Weapon",
+    value: "Has Exalted Weapon",
+    description: "Warframes that have exalted weapons",
     extra: ["Pseudo-Exalted weapons count towards this category"],
   }),
-  leverian: (value: boolean) => ({
-    //TODO: Label needs to be updated
-    category: "Leverian",
-    value: value,
+  leverian: (_: boolean) => ({
+    header: "Leverian",
+    value: "Has Entry",
     description: "Warframes that have a Leverian Entry",
   }),
-  protoframe: (value: boolean) => ({
+  protoframe: (_: boolean) => ({
     //TODO: Label needs to be updated
-    category: "Protoframe",
-    value: value,
+    header: "Protoframe",
+    value: "Protoframe",
     description: "Warframes with a Protoframe that possess their traits",
   }),
-  questframe: (value: boolean) => ({
-    //TODO: Label needs to be updated
-    category: "Acquisition",
-    value: value,
+  questframe: (_: boolean) => ({
+    header: "Acquisition",
+    value: "Quest Locked",
     description: "Warframes not obtainable until completing a specific quest.",
     extra: ["Based on Warframe Unlock Quests on the Wiki"],
   }),
-  circuit: (value: boolean) => ({
-    //TODO: Label needs to be updated
-    category: "The Circuit",
-    value: value,
+  circuit: (_: boolean) => ({
+    header: "Acquisition",
+    value: "The Circuit",
     description: "Warframes that can obtained through the Circuit",
   }),
 };
 
 export type CategoryName = keyof typeof categoryMetadata;
+
+// Not fully sure how these work but they allow me to access the keys of categoryMetadata without TS errors
+
+type CategoryValueMap = {
+  [K in CategoryName]: Parameters<(typeof categoryMetadata)[K]>[0];
+};
+
+export function getCategoryDisplay<K extends CategoryName>(
+  key: K,
+  value: CategoryValueMap[K],
+) {
+  const metadataFn = categoryMetadata[key];
+
+  if (!metadataFn) return null;
+
+  return (metadataFn as (val: CategoryValueMap[K]) => CategoryMetadata)(value);
+}

89da2823b97294e32b4b9d6ab27ff379a3a72bbc
kadeem-lewis <email@kadeemlewis.com>
2026-01-13 05:27:00 -0500

    database migration

---
+++ server/database/migrations/0005_bumpy_echo.sql
@@ -0,0 +1,3 @@
+ALTER TABLE "categories" DROP COLUMN "label";--> statement-breakpoint
+ALTER TABLE "categories" DROP COLUMN "description";--> statement-breakpoint
+ALTER TABLE "categories" DROP COLUMN "type";

89f704fbc3db1dc791fd069affa5e610595eaef4
kadeem-lewis <email@kadeemlewis.com>
2026-01-13 05:16:25 -0500

    updated label to showcase data based on specifications from category metadata

--- app/components/grid/Label.vue
+++ app/components/grid/Label.vue
@@ -1,19 +1,62 @@
 <script setup lang="ts">
+import { getCategoryDisplay } from "#shared/data/categoryMetadata";
+import { getKeyValueFromId } from "#shared/utils/grid";
+
 const { category } = defineProps<{
-  category: CategoryItem;
+  category: string;
 }>();
+
+const { key, value } = getKeyValueFromId(category);
+
+const categoryInfo = computed(() => getCategoryDisplay(key, value));
+
+console.log(key, value, categoryInfo.value, category);
+
+getCategoryDisplay(key, value);
 </script>
 <template>
   <UPopover
     :ui="{
-      content: 'rounded-none',
+      content: 'rounded-none max-w-96',
     }"
   >
-    <UButton variant="ghost" class="min-h-20 rounded-none">{{
-      category.label
-    }}</UButton>
+    <UButton variant="ghost" class="flex min-h-28 rounded-none">
+      <div class="flex flex-col items-center gap-0.5">
+        <p class="text-toned text-xs font-semibold uppercase">
+          {{ categoryInfo?.header }}
+        </p>
+        <NuxtImg
+          v-if="categoryInfo?.imgSrc"
+          format="avif"
+          :src="categoryInfo.imgSrc"
+          :alt="categoryInfo?.value"
+          class="size-8"
+          :class="{
+            'dark:invert': categoryInfo.header === 'Aura',
+            'invert dark:invert-0': categoryInfo.header === 'Playstyle',
+          }"
+        />
+        <p :class="[categoryInfo?.cssClass]">
+          {{ categoryInfo?.value }}
+        </p>
+      </div>
+    </UButton>
     <template #content>
-      <p class="p-2">{{ category.description }}</p>
+      <div class="p-2">
+        <p class="p-2">{{ categoryInfo?.description }}</p>
+        <ul v-if="categoryInfo?.extra">
+          <li
+            v-for="(item, index) in categoryInfo.extra"
+            :key="index"
+            class="flex items-center gap-1 break-keep"
+          >
+            <span>■</span>
+            <span>
+              {{ item }}
+            </span>
+          </li>
+        </ul>
+      </div>
     </template>
   </UPopover>
 </template>

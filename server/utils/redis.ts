9a168e473e61eb0586ace4780beb8f11c649bb33
kadeem-lewis <email@kadeemlewis.com>
2026-01-17 02:51:29 -0500

    updated redis config to handle reconnecting better

--- server/utils/redis.ts
+++ server/utils/redis.ts
@@ -5,9 +5,6 @@ let _client: ReturnType<typeof createClient> | null = null;
 export const useRedis = async () => {
   const runtimeConfig = useRuntimeConfig();
 
-  if (_client && _client.isOpen) {
-    return _client;
-  }
   if (!_client) {
     _client = createClient({
       url: runtimeConfig.redis.url,
@@ -22,7 +19,17 @@ export const useRedis = async () => {
 
     _client.on("error", (err) => console.error("Redis Client Error", err));
 
-    await _client.connect();
+    if (!_client.isOpen) {
+      try {
+        await _client.connect();
+      } catch (e) {
+        if (e instanceof Error && e.message === "Socket already opened") {
+          // Safe to ignore, do nothing
+        } else {
+          throw e;
+        }
+      }
+    }
   }
   return _client;
 };

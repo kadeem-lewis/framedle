b326df08047e03198bed10fcb194a321c9fac2fc
kadeem-lewis <email@kadeemlewis.com>
2026-01-17 02:44:34 -0500

    updated redis to use clientSide caching

--- server/utils/redis.ts
+++ server/utils/redis.ts
@@ -11,6 +11,13 @@ export const useRedis = async () => {
   if (!_client) {
     _client = createClient({
       url: runtimeConfig.redis.url,
+      RESP: 3,
+      clientSideCache: {
+        ttl: 60 * 60 * 1000,
+        maxEntries: 5000,
+
+        evictPolicy: "LRU",
+      },
     });
 
     _client.on("error", (err) => console.error("Redis Client Error", err));

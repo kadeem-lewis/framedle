f2694a4795a55877e4fa0609980f65c4f1812bc4
kadeem-lewis <email@kadeemlewis.com>
2026-02-24 22:28:50 -0500

    created pages for faqs and patch notes and added links to footer

---
+++ app/pages/changelog.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts"></script>
+<template>
+  <div>
+    <h2 class="text-xl font-semibold">Change log</h2>
+  </div>
+</template>
---
+++ app/pages/faqs.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import type { AccordionItem } from "@nuxt/ui";
+const items = shallowRef<AccordionItem[]>([
+  {
+    label: "Q: When do new puzzles come out?",
+    content: "New puzzles are released at midnight local time.",
+  },
+]);
+</script>
+<template>
+  <section class="flex flex-col gap-4">
+    <h2 class="text-xl font-semibold">Frequently Asked Questions</h2>
+    <UAccordion :items />
+  </section>
+</template>

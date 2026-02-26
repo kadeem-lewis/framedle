77c1cad9846a9e4c6eaf932c2ae4706ab3d8c8b8
kadeem-lewis <email@kadeemlewis.com>
2026-02-25 19:16:04 -0500

    changed backgrounds from being partially transparent

--- app/pages/classic/[...path].vue
+++ app/pages/classic/[...path].vue
@@ -102,7 +102,7 @@ const { makeGuess } = useGuess();
         <template v-if="guessedItems[mode].length && itemToGuess[mode]">
           <div class="space-y-4 overflow-x-auto md:overflow-x-visible">
             <div
-              class="grid w-[190%] grid-cols-8 gap-1 border border-neutral-200 bg-white/75 py-0.5 text-sm uppercase md:-ml-[45%] md:text-base dark:border-neutral-800 dark:bg-neutral-900/75"
+              class="grid w-[190%] grid-cols-8 gap-1 border border-neutral-200 bg-white py-0.5 text-sm uppercase md:-ml-[45%] md:text-base dark:border-neutral-800 dark:bg-neutral-900"
             >
               <UTooltip
                 v-for="label of feedbackLabels"

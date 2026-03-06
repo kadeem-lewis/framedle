d5739392aed5ed458277227ad1432f3536818200
kadeem-lewis <email@kadeemlewis.com>
2026-03-06 16:02:55 -0500

    removed h1 from logo and unnecessary styles

--- app/components/TheNavbar.vue
+++ app/components/TheNavbar.vue
@@ -6,20 +6,16 @@ const { openDialog } = useDialog();
     class="w-full border-b-2 border-neutral-500 py-2 dark:border-neutral-700"
   >
     <UContainer class="flex items-center justify-between">
-      <div>
-        <h1 class="font-ailerons flex flex-row gap-2 text-[42px] leading-none">
-          <NuxtLink to="/" aria-label="Back to home">
-            <TheLogo />
-          </NuxtLink>
-        </h1>
-      </div>
+      <NuxtLink to="/" aria-label="Back to home">
+        <TheLogo />
+      </NuxtLink>
       <div class="flex items-center gap-1">
         <UButton
           aria-label="settings"
           icon="i-heroicons-cog-6-tooth-solid"
           variant="outline"
           square
-          class="group hover:border-primary dark:border-primary border-b-4 border-neutral-800"
+          class="group border-b-4 border-neutral-800 hover:border-primary dark:border-primary"
           :ui="{
             leadingIcon: 'group-hover:rotate-45 transition-transform',
           }"

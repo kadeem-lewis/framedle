d5067c58052df062d03d787aaac7dad9c7d6a9ef
kadeem-lewis <email@kadeemlewis.com>
2026-03-07 17:57:56 -0500

    created new styles for neutral variant of tenno button and updated app config to use sorting

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,3 +1,5 @@
+import { tw } from "~/utils/tw";
+
 export default defineAppConfig({
   ui: {
     colors: {
@@ -23,14 +25,25 @@ export default defineAppConfig({
         {
           color: "primary",
           variant: "outline",
-          class:
-            "ring-neutral-800 dark:ring-primary bg-white/75 dark:bg-neutral-900/75 hover-gradient text-neutral-700 dark:text-primary hover:ring-primary hover:text-primary",
+          class: tw`bg-white/75 hover-gradient text-neutral-700 ring-neutral-800 hover:text-primary hover:ring-primary dark:bg-neutral-900/75 dark:text-primary dark:ring-primary`,
         },
         {
           color: "primary",
           variant: "tenno",
-          class:
-            "font-bold dark:text-primary border border-accented dark:border-default border-b-2 border-b-neutral-400 dark:border-b-primary bg-neutral-100/70 dark:bg-neutral-950 hover:border-primary-400 dark:hover:border-accented hover:border-b-primary-500 dark:hover:border-b-primary-300 hover:text-primary hover-gradient",
+          class: tw`border border-b-2 border-accented border-b-neutral-400 bg-neutral-100/70 hover-gradient font-bold hover:border-primary-400 hover:border-b-primary-500 hover:text-primary dark:border-default dark:border-b-primary dark:bg-neutral-950 dark:text-primary dark:hover:border-accented dark:hover:border-b-primary-300`,
+        },
+        {
+          color: "neutral",
+          variant: "tenno",
+          class: tw`border border-b-3 border-accented border-b-neutral-400 bg-default hover-gradient hover:border-b-primary-600 hover:text-primary-600 dark:border-b-neutral-500 dark:hover:border-accented dark:hover:border-b-primary dark:hover:text-primary`,
+        },
+        {
+          color: "neutral",
+          variant: "tenno",
+          leading: true,
+          class: {
+            leadingIcon: tw`text-default group-hover:text-primary`,
+          },
         },
       ],
     },
@@ -43,9 +56,8 @@ export default defineAppConfig({
       variants: {
         variant: {
           tenno: {
-            list: "rounded-none bg-default p-0 border-default uppercase dark:bg-elevated",
-            trigger:
-              "grow  rounded-none flex items-start data-[state=active]:bg-default data-[state=active]:border-b-4",
+            list: tw`rounded-none border-default bg-default p-0 uppercase dark:bg-elevated`,
+            trigger: tw`flex grow items-start rounded-none data-[state=active]:border-b-4 data-[state=active]:bg-default`,
             indicator: "rounded-none",
             label: "uppercase font-semibold",
           },
@@ -62,8 +74,7 @@ export default defineAppConfig({
           variant: "tenno",
           class: {
             label: "dark:text-primary",
-            trigger:
-              "dark:border-primary dark:hover:data-[state=active]:border-primary-300 dark:hover:text-primary-300",
+            trigger: tw`dark:border-primary dark:hover:text-primary-300 dark:hover:data-[state=active]:border-primary-300`,
           },
         },
       ],

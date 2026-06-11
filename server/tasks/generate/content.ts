e49292415533b4b5f9015d181793f3e0219bf0f5
kadeem-lewis <email@kadeemlewis.com>
2026-06-10 20:14:26 -0400

    updated generate:content to call generate:queue after running

--- server/tasks/generate/content.ts
+++ server/tasks/generate/content.ts
@@ -12,27 +12,36 @@ export default defineTask({
     description: "Generate the warframe and ability data used in app",
   },
   async run() {
-    const data = await fetchWarframeData();
-    const warframes = buildWarframeData(data);
-    const abilities = await buildAbilityData(warframes);
+    try {
+      const data = await fetchWarframeData();
+      const warframes = buildWarframeData(data);
+      const abilities = await buildAbilityData(warframes);
 
-    const updatedWarframes = updateWarframeDataFromAbilities(
-      warframes,
-      abilities,
-    );
+      const updatedWarframes = updateWarframeDataFromAbilities(
+        warframes,
+        abilities,
+      );
 
-    await writeDataToFile(updatedWarframes, abilities);
+      await writeDataToFile(updatedWarframes, abilities);
 
-    const payload = {
-      warframeNames: Object.keys(updatedWarframes),
-      abilityNames: Object.keys(abilities),
-    };
+      const payload = {
+        warframeNames: Object.keys(updatedWarframes),
+        abilityNames: Object.keys(abilities),
+      };
 
-    //! I can't run the queue yet because I am still connected to the production db and that would push unwanted changes
+      await runTask("generate:queue", {
+        payload,
+      });
 
-    return {
-      result: "Success",
-      builtWarframeData: Object.fromEntries(updatedWarframes.entries()),
-    };
+      return {
+        result: "Success",
+      };
+    } catch (error) {
+      console.error("Error generating content:", error);
+      return {
+        result: "Error",
+        error,
+      };
+    }
   },
 });

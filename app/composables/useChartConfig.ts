9d4955d97517d6164aa52aec1724413ab32c9bac
kadeem-lewis <email@kadeemlewis.com>
2026-06-12 04:19:48 -0400

    added non-null assertions to chartConfig to resolve type errors

--- app/composables/useChartConfig.ts
+++ app/composables/useChartConfig.ts
@@ -4,6 +4,8 @@ export function useChartConfig(chartRef: Ref<HTMLElement | null>) {
   const primaryColor = useCssVar("--ui-primary", chartRef);
   const textColor = useCssVar("--ui-text", chartRef);
 
+  // These values can be undefined and it is causing ts errors for ApexOptions since colors can't be undefined there.
+
   const baseOptions = computed<ApexOptions>(() => ({
     chart: {
       toolbar: {
@@ -42,14 +44,15 @@ export function useChartConfig(chartRef: Ref<HTMLElement | null>) {
     grid: {
       show: false,
     },
+    // I am asserting that useCSSVars is not undefined which I don't think it can be since it should always be defined in the CSS, but I need to do this to satisfy the ApexOptions type.
     dataLabels: {
       enabled: true,
       style: {
         fontFamily: "Noto Sans, sans-serif",
-        colors: [textColor.value],
+        colors: [textColor.value!],
       },
     },
-    colors: [primaryColor.value],
+    colors: [primaryColor.value!],
   }));
   return {
     baseOptions,

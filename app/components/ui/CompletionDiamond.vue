bedcd0ddd2e8ac3fb44cb1ef45dd99e62f9c41b2
kadeem-lewis <email@kadeemlewis.com>
2026-07-25 04:04:20 -0400

    recreated completion markers from warframe

---
+++ app/components/ui/CompletionDiamond.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const { isFilled } = defineProps<{
+  isFilled: boolean;
+}>();
+</script>
+<template>
+  <div class="size-4 rotate-45 border-2 border-primary">
+    <div v-if="isFilled" class="absolute inset-0 m-auto size-2 bg-primary" />
+  </div>
+</template>
+<style scoped></style>
